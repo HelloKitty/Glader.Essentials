b94cd79d14cb6c6e12ce124b3a9c45d2c58f5c26
HelloKitty <andrew.blakely@ymail.com>
2019-03-13 17:41:19 -0500

    Added Attributes and Common IoC Modules

---
+++ src/Glader.Essentials.Unity/Attributes/SceneTypeCreateAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Metadata attributed used to indicate that
+	/// a particular object should be created in a specified Scene type.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public abstract class SceneTypeCreateAttribute : Attribute
+	{
+		/// <summary>
+		/// Indicates the scene type this handler is for.
+		/// </summary>
+		public int SceneType { get; }
+
+		/// <inheritdoc />
+		protected SceneTypeCreateAttribute(int sceneType)
+		{
+			if(sceneType < 0) throw new ArgumentOutOfRangeException(nameof(sceneType));
+
+			SceneType = sceneType;
+		}
+	}
+}
