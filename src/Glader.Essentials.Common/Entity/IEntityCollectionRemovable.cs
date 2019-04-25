4f4d1102db1d867240923179df4aa6bfc173fa45
HelloKitty <andrew.blakely@ymail.com>
2019-04-25 09:48:54 -0500

    Added EntityGuidMappable system

---
+++ src/Glader.Essentials.Common/Entity/IEntityCollectionRemovable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for collections that can have entries
+	/// removed via a <see cref="TKey"/>.
+	/// </summary>
+	public interface IEntityCollectionRemovable<in TKey>
+	{
+		/// <summary>
+		/// Attempts to remove the collection based
+		/// on the provided <see cref="entityGuid"/> key.
+		/// </summary>
+		/// <param name="entityGuid">The key.</param>
+		/// <returns>True if removed.</returns>
+		bool RemoveEntityEntry(TKey entityGuid);
+	}
+}
---
+++ src/Glader.Essentials.Common/Modules/EntityMappableRegisterationModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Glader.Essentials
+{
+	public sealed class EntityMappableRegisterationModule<TEntityKey> : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			//The below is kinda a hack to register the non-generic types in the
+			//removabale collection
+			List<IEntityCollectionRemovable<TEntityKey>> removableComponentsList = new List<IEntityCollectionRemovable<TEntityKey>>(10);
+
+			builder.RegisterGeneric(typeof(EntityGuidDictionary<,>))
+				.AsSelf()
+				.As(typeof(IReadonlyEntityGuidMappable<,>))
+				.As(typeof(IEntityGuidMappable<,>))
+				.OnActivated(args =>
+				{
+					if(args.Instance is IEntityCollectionRemovable<TEntityKey> removable)
+						removableComponentsList.Add(removable);
+				})
+				.SingleInstance();
+
+			//This will allow everyone to register the removable collection collection.
+			builder.RegisterInstance(removableComponentsList)
+				.As<IReadOnlyCollection<IEntityCollectionRemovable<TEntityKey>>>()
+				.AsSelf()
+				.SingleInstance();
+		}
+	}
+}
