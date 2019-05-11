3599c74935504e51aea42acdde11e64e26f25d51
HelloKitty <andrew.blakely@ymail.com>
2019-05-10 19:11:15 -0500

    Supported Typed Keyed UI Registerations

--- src/Glader.Essentials.Unity/Modules/UIDependencyRegisterationModule.cs
+++ src/Glader.Essentials.Unity/Modules/UIDependencyRegisterationModule.cs
@@ -3,24 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Autofac;
+using Generic.Math;
 using SceneJect.Common;
 using UnityEngine.SceneManagement;
 
 namespace Glader.Essentials
 {
-	public sealed class UIDependencyRegisterationModule : Module
+	public class UIDependencyRegisterationModule<TKeyType> : Module
 	{
 		/// <inheritdoc />
 		protected override void Load(ContainerBuilder register)
 		{
-			//Because of active load scene, we have to iterate each scene
-			foreach(var registerable in new SceneUiElementEnumerable())
+			if(typeof(TKeyType) == typeof(int))
 			{
-				//Registers the adapter with the specified Key and Service Type.
-				register.RegisterInstance(registerable)
-					.SingleInstance()
-					.Keyed(registerable.RegisterationKey, registerable.UISerivdeType);
+				//Because of active load scene, we have to iterate each scene
+				foreach(var registerable in new SceneUiElementEnumerable())
+				{
+					//Registers the adapter with the specified Key and Service Type.
+					register.RegisterInstance(registerable)
+						.SingleInstance()
+						.Keyed(registerable.RegisterationKey, registerable.UIServiceType);
+				}
+			}
+			else
+			{
+				//Because of active load scene, we have to iterate each scene
+				foreach(var registerable in new SceneUiElementEnumerable())
+				{
+					//Registers the adapter with the specified Key and Service Type.
+					register.RegisterInstance(registerable)
+						.SingleInstance()
+						.Keyed(GenericMath.Convert<int, TKeyType>(registerable.RegisterationKey), registerable.UIServiceType);
+				}
 			}
 		}
+	}
+	
+	/// <summary>
+	/// Default int-keyed implementation of <see cref="UIDependencyRegisterationModule{TKeyType}"/>.
+	/// </summary>
+	public sealed class UIDependencyRegisterationModule : UIDependencyRegisterationModule<int>
+	{
+		
 	}
 }
