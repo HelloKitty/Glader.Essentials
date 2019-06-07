aab51646ad1dfe6198d7777c604a2f10e37e0351
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 05:35:45 -0500

    Added IGameStartable for Post-Initialization work ONE TIME

---
+++ src/Glader.Essentials.Common/Engine/IGameStartable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glader
+{
+	/// <summary>
+	/// Contracts for types that require being informed when
+	/// the game actually starts.
+	/// </summary>
+	public interface IGameStartable
+	{
+		/// <summary>
+		/// Called when the game starts.
+		/// Should not be used for initialization.
+		/// </summary>
+		Task Start();
+	}
+}
