00b1f064008570bd6bf905c2822cafc6cde4aeff
HelloKitty <andrew.blakely@ymail.com>
2019-06-30 19:44:54 -0500

    Rewrote EntityGuidMappable API

--- src/Glader.Essentials.Common/Entity/IEntityGuidMappable.cs
+++ src/Glader.Essentials.Common/Entity/IEntityGuidMappable.cs
@@ -4,8 +4,24 @@ using System.Text;
 
 namespace Glader.Essentials
 {
-	public interface IEntityGuidMappable<TKeyType, TValue> : IDictionary<TKeyType, TValue>, IEntityCollectionRemovable<TKeyType>
+	public interface IEntityGuidMappable<in TKeyType, TValueType> : IReadonlyEntityGuidMappable<TKeyType, TValueType>, IEntityCollectionRemovable<TKeyType>
 	{
+		/// <summary>
+		/// Gets the element that has the specified key in the read-only dictionary.
+		/// </summary>
+		/// <param name="key">The key to locate.</param>
+		/// <exception cref="ArgumentNullException">key is null.</exception>
+		/// <exception cref="KeyNotFoundException">The property is retrieved and key is not found.</exception>
+		/// <returns>The element that has the specified key in the read-only dictionary.</returns>
+		new TValueType this[TKeyType key] { get; set; }
 
+		/// <summary>
+		/// Adds an element with the provided key and value to the <see cref="IEntityGuidMappable{TKeyType,TValueType}"/>
+		/// </summary>
+		/// <param name="key">The object to use as the key of the element to add.</param>
+		/// <param name="value">The object to use as the value of the element to add.</param>
+		/// <exception cref="ArgumentNullException">key is null.</exception>
+		/// <exception cref="ArgumentException">An element with the same key already exists in the <see cref="IEntityGuidMappable{TKeyType,TValueType}"/></exception>
+		void Add(TKeyType key, TValueType value);
 	}
 }
