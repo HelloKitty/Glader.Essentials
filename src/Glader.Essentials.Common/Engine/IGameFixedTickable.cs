662545cdc0dfc0a4311fdf07de563a0239407ba2
HelloKitty <andrew.blakely@ymail.com>
2019-09-22 20:25:20 -0500

    Added IGameFixedTickable

---
+++ src/Glader.Essentials.Common/Engine/IGameFixedTickable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for services/components that need to be ticked
+	/// forward in time in a fixed way.
+	/// </summary>
+	public interface IGameFixedTickable
+	{
+		/// <summary>
+		/// Called to in a fixed time step.
+		/// </summary>
+		void OnGameFixedTick();
+	}
+}
