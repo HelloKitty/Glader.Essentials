e306a82e466fe9a384395c8167f03142eb84531e
HelloKitty <andrew.blakely@ymail.com>
2019-10-06 20:44:30 -0500

    Added IUIParentable

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/UnityParentableUIAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Glader
+{
+	public sealed class UnityParentableUIAdapter : BaseUnityUIAdapter<Transform, IUIParentable>, IUIParentable
+	{
+		public void Parent([NotNull] GameObject go)
+		{
+			if (go == null) throw new ArgumentNullException(nameof(go));
+
+			go.transform.parent = this.UnityUIObject;
+		}
+	}
+}
