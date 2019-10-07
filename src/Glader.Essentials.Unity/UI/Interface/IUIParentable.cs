ce9300cc579223a836e1d5b562faa001adfc9163
HelloKitty <andrew.blakely@ymail.com>
2019-10-06 20:44:30 -0500

    Added IUIParentable

---
+++ src/Glader.Essentials.Unity/UI/Interface/IUIParentable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader
+{
+	public interface IUIParentable
+	{
+		/// <summary>
+		/// Parents the provided <see cref="go"/> to
+		/// the target <see cref="IUIParentable"/>.
+		/// </summary>
+		/// <param name="go">The game object to parent.</param>
+		void Parent(GameObject go);
+	}
+}
