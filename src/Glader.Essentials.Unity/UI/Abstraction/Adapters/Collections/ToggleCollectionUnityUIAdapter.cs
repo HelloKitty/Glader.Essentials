74342272ad1b654271f79a6245f4c641fd442262
HelloKitty <andrew.blakely@ymail.com>
2020-05-21 21:59:09 -0500

    Added ToggleCollectionUnityUIAdapter

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Collections/ToggleCollectionUnityUIAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader
+{
+	public sealed class ToggleCollectionUnityUIAdapter : BaseSerializeableUnityUICollectionAdapter<IUIToggle, UnityToggleUIToggleAdapter>
+	{
+		protected override IUIToggle[] InterfaceTypes()
+		{
+			return SerializedElements;
+		}
+	}
+}
