2d2450e03987a123fe47cf10106d0600e6834f46
HelloKitty <andrew.blakely@ymail.com>
2020-05-21 21:58:26 -0500

    Added BaseSerializeableUnityUICollectionAdapter

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Collections/BaseSerializeableUnityUICollectionAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public abstract class BaseSerializeableUnityUICollectionAdapter<TInterfaceType, ImplementationType> : BaseCollectionUnityUIAdapter<TInterfaceType>
+		where ImplementationType  : TInterfaceType
+	{
+		[SerializeField]
+		[Tooltip("The collection of toggles to aggregate.")]
+		private ImplementationType[] _elements;
+
+		/// <inheritdoc />
+		protected override IEnumerable<TInterfaceType> Elements => InterfaceTypes();
+
+		//The below is a hack that exists to get around generic co/contra variance issues.
+		protected ImplementationType[] SerializedElements => _elements;
+
+		protected abstract TInterfaceType[] InterfaceTypes();
+	}
+}
