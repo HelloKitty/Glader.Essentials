c6ee06144d75d8b3f243e6bf784cecaa66354ea3
HelloKitty <andrew.blakely@ymail.com>
2020-09-16 14:41:38 -0500

    Fixed issues allocating sprites

--- src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Base/BaseUnityUIImageAdapter.cs
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Base/BaseUnityUIImageAdapter.cs
@@ -12,13 +12,8 @@ namespace Glader.Essentials
 		/// <inheritdoc />
 		public virtual void SetSpriteTexture(Texture2D texture)
 		{
-			if(UnityUIObject.sprite == null)
-				UnityUIObject.sprite = Sprite.Create(texture, Rect.zero, Vector2.zero); //TODO: What should defaults be?
-			else
-			{
-				//Sprites complain if we don't have proper size, so we need size based on the texture2D
-				UnityUIObject.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-			}
+			//Sprites complain if we don't have proper size, so we need size based on the texture2D
+			UnityUIObject.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
 		}
 	}
 }
