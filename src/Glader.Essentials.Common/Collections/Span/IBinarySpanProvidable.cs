051cf182202231169c45123df15e74b52b4d0ea9
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 01:15:08 -0600

    Completely reorganized repository

---
+++ src/Glader.Essentials.Common/Collections/Span/IBinarySpanProvidable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// <see cref="byte"/> implementation of <see cref="ISpanProvidable{T}"/>
+	/// </summary>
+	public interface IBinarySpanProvidable : ISpanProvidable<byte>
+	{
+
+	}
+}
---
+++ src/Glader.Essentials.Common/Collections/Span/ISpanProvidable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contracts for types that can provide a <see cref="Span{T}"/>.
+	/// Generally used to represent the object as a <see cref="Span{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The element type of the span.</typeparam>
+	public interface ISpanProvidable<T>
+	{
+		/// <summary>
+		/// Provides a <see cref="Span{T}"/>.
+		/// </summary>
+		/// <returns>A valid <see cref="Span{T}"/></returns>
+		Span<T> GetSpan();
+	}
+}
