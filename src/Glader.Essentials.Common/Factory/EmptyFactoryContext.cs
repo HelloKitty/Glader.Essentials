d2388d925d39bcad3b4b97085f5ad49aba037f64
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 01:15:08 -0600

    Completely reorganized repository

---
+++ src/Glader.Essentials.Common/Factory/EmptyFactoryContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Represents an empty factory creation context.
+	/// For usage with <see cref="IFactoryCreatable{TCreateType,TContextType}"/>.
+	/// </summary>
+	public sealed class EmptyFactoryContext
+	{
+		/// <summary>
+		/// Singleton empty creation context.
+		/// </summary>
+		public static EmptyFactoryContext Instance { get; } = new EmptyFactoryContext();
+
+		/// <summary>
+		/// Enforces initialization order.
+		/// Do not remove.
+		/// </summary>
+		static EmptyFactoryContext()
+		{
+
+		}
+
+		/// <summary>
+		/// Private ctor makes it un-constructable outside of the class.
+		/// </summary>
+		private EmptyFactoryContext()
+		{
+
+		}
+	}
+}
---
+++ src/Glader.Essentials.Common/Factory/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader
+{
+	/// <summary>
+	/// Extension methods for <see cref="IFactoryCreatable{TCreateType,TContextType}"/>.
+	/// </summary>
+	public static class IFactoryCreatableExtensions
+	{
+		/// <summary>
+		/// Creates a new instance of the <typeparamref name="TCreateType"/>.
+		/// (Simplified overload for <see cref="EmptyFactoryContext"/>-based factories.
+		/// </summary>
+		/// <returns>A new instance.</returns>
+		public static TCreateType Create<TCreateType>(this IFactoryCreatable<TCreateType, EmptyFactoryContext> factory)
+		{
+			if (factory == null) throw new ArgumentNullException(nameof(factory));
+
+			return factory.Create(EmptyFactoryContext.Instance);
+		}
+	}
+}
