6842bd4663e38e55a585f6a186ed5039f65c52e2
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 00:08:24 -0600

    Added ProducesJsonAttribute for ASP

---
+++ src/Glader.Essentials.ASP/Attribute/ProducesJsonAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Marks a controller/action indicating it should produce JSON
+	/// content.
+	/// </summary>
+	public sealed class ProducesJsonAttribute : ProducesAttribute
+	{
+		/// <summary>
+		/// The content-type used by the attribute.
+		/// </summary>
+		public static string JsonContentType { get; } = "application/json";
+
+		/// <inheritdoc />
+		public ProducesJsonAttribute() 
+			: base(JsonContentType)
+		{
+
+		}
+	}
+}
