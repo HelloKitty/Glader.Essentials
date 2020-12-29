e959817a0a6283ed14338ecf62db062c517b5120
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 22:54:06 -0600

    Rewrote ClaimsReader extensions

--- src/Glader.Essentials.ASP/Extensions/IClaimsPrincipalReaderExtensions.cs
+++ src/Glader.Essentials.ASP/Extensions/IClaimsPrincipalReaderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Security.Claims;
 using System.Text;
 
@@ -15,7 +16,7 @@ namespace Glader.Essentials
 		/// <returns>The User ID claim value. Will throw if the principal doesn't contain the id.</returns>
 		/// <exception cref="ArgumentException">Throws if the provided principal doesn't contain an id.</exception>
 		/// <remarks>The User ID claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
-		public static int GetAccountIdInt(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+		public static int GetAccountId(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
 		{
 			if(!int.TryParse(reader.GetAccountId(principal), out var accountId))
 				throw new ArgumentException($"Provided {nameof(ClaimsPrincipal)} does not contain a user id.", nameof(principal));
@@ -23,15 +24,32 @@ namespace Glader.Essentials
 			return accountId;
 		}
 
-		//TODO: Doc.
-		public static int GetPlayerAccountId(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+		/// <summary>
+		/// Returns the User ID claim value if present otherwise will throw.
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <param name="principal">The <see cref="ClaimsPrincipal"/> instance.</param>
+		/// <returns>The User ID claim value. Will throw if the principal doesn't contain the id.</returns>
+		/// <exception cref="ArgumentException">Throws if the provided principal doesn't contain an id.</exception>
+		/// <remarks>The User ID claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
+		public static TIdentifierType GetAccountId<TIdentifierType>(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
 		{
-			string accountIdString = principal.FindFirstValue("sub");
+			try
+			{
+				string id = reader.GetAccountId(principal);
 
-			if(int.TryParse(accountIdString, out int resultValue))
-				return resultValue;
-			else
-				throw new InvalidOperationException($"Failed to read Player AccountId Claim: {"sub"}");
+				if(typeof(TIdentifierType) == typeof(string))
+					return (TIdentifierType)(object)id;
+
+				if(id == default)
+					return default;
+
+				return (TIdentifierType)Convert.ChangeType(id, typeof(TIdentifierType));
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentException($"Provided {nameof(ClaimsPrincipal)} does not contain a user id.", nameof(principal), e);
+			}
 		}
 	}
 }
