3880d9a3211df051e71647aa456703c947be7547
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 22:56:44 -0600

    Added GladerASPIServiceCollectionExtensions

---
+++ src/Glader.Essentials.ASP/Extensions/GladerASPIServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Glader.Essentials
+{
+	public static class GladerASPIServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Registers all the required/related Glader ASP services.
+		/// </summary>
+		/// <param name="serviceCollection"></param>
+		/// <returns></returns>
+		public static IServiceCollection RegisterGladerASP(this IServiceCollection serviceCollection)
+		{
+			if(serviceCollection == null) throw new ArgumentNullException(nameof(serviceCollection));
+
+			serviceCollection.AddSingleton<IClaimsPrincipalReader, ClaimsPrincipalReader>();
+
+			return serviceCollection;
+		}
+	}
+}
