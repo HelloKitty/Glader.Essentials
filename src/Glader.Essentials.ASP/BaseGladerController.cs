540304fbb4d7a56bd9a8d9e3cbca18da68a3e07b
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 22:48:27 -0600

    Added Glader ASP Library

---
+++ src/Glader.Essentials.ASP/BaseGladerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// The base controller Type for Glader ASP MVC controllers.
+	/// </summary>
+	public abstract class BaseGladerController : Controller
+	{
+		/// <summary>
+		/// The logging service for the controller.
+		/// </summary>
+		protected ILogger<BaseGladerController> Logger { get; }
+
+		/// <inheritdoc />
+		protected BaseGladerController([FromServices] ILogger<BaseGladerController> logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		//TODO: Fix doc
+		/// <summary>
+		/// Builds a successful JsonResult of ResponseModel
+		/// with a successful result code an the value of result.
+		/// </summary>
+		/// <typeparam name="TModelType">The model type.</typeparam>
+		/// <param name="result">The result.</param>
+		/// <returns></returns>
+		protected JsonResult BuildSuccessfulResponseModel<TModelType>(TModelType result)
+			where TModelType : class
+		{
+			if (result == null) throw new ArgumentNullException(nameof(result));
+			return Json(new ResponseModel<TModelType, StubbedResponseCode>(result)); //We use a stubbed response code since we don't actually known the result.
+		}
+
+		/// <summary>
+		/// TODO: Fix doc, intellisense not working as I'm writing this.
+		/// </summary>
+		/// <typeparam name="TResponseCodeType">The response code type.</typeparam>
+		/// <param name="failedResponseCode">The failure code.</param>
+		/// <returns></returns>
+		protected JsonResult BuildFailedResponseModel<TResponseCodeType>(TResponseCodeType failedResponseCode)
+			where TResponseCodeType : Enum
+		{
+			//TODO: add enum check.
+			return Json(new ResponseModel<object, TResponseCodeType>(failedResponseCode)); //We use a stubbed response code since we don't actually known the result.
+		}
+	}
+}
---
+++ src/Glader.Essentials.ASP/Security/ClaimsPrincipalReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Glader.Essentials
+{
+	//From HaloLive: https://github.com/HaloLive/HaloLive.Library/tree/9ca485677a8c6f85bf06de53193af704aa508dcd/src/HaloLive.Hosting.Authorization.Server/Services
+	public sealed class ClaimsPrincipalReader : IClaimsPrincipalReader
+	{
+		/// <summary>
+		/// The <see cref="IdentityOptions"/> used to configure Identity.
+		/// </summary>
+		private IdentityOptions Options { get; }
+
+		public ClaimsPrincipalReader(IOptions<IdentityOptions> options)
+		{
+			//We don't allow defaults like ASP implementation does
+			if(options == null) throw new ArgumentNullException(nameof(options));
+
+			Options = options.Value;
+		}
+
+		/// <inheritdoc />
+		public string GetAccountId(ClaimsPrincipal principal)
+		{
+			if(principal == null)
+			{
+				throw new ArgumentNullException(nameof(principal));
+			}
+			return principal.FindFirstValue(Options.ClaimsIdentity.UserIdClaimType);
+		}
+
+		/// <inheritdoc />
+		public string GetAccountName(ClaimsPrincipal principal)
+		{
+			if(principal == null)
+			{
+				throw new ArgumentNullException(nameof(principal));
+			}
+			return principal.FindFirstValue(Options.ClaimsIdentity.UserNameClaimType);
+		}
+
+		/// <inheritdoc />
+		public string GetGloballyUniqueUserId(ClaimsPrincipal principal)
+		{
+			if(principal == null) throw new ArgumentNullException(nameof(principal));
+
+			Claim claim = principal.FindFirst("jti");
+
+			if(claim == null)
+				throw new InvalidOperationException($"The principal does not contain a claim for GUID/UUID (JTI).");
+
+			return claim.Value;
+		}
+	}
+}
