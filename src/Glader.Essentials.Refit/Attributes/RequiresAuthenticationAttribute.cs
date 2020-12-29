c5e1605f12b557c09c035a91f307502369d74a3e
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 22:27:02 -0600

    Added Essentials.Refit

---
+++ src/Glader.Essentials.Refit/Attributes/RequiresAuthenticationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Attribute that indicates an authentication header will be used or is required.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+	public sealed class RequiresAuthenticationAttribute : HeadersAttribute
+	{
+		public RequiresAuthenticationAttribute()
+			: base("Authorization: temp") //t for temp, will be replaced.
+		{
+		}
+	}
+}
---
+++ src/Glader.Essentials.Refit/Token/IAuthTokenRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public interface IAuthTokenRepository : IReadonlyAuthTokenRepository
+	{
+		/// <summary>
+		/// Sets a new <see cref="authToken"/>.
+		/// Should be non-null.
+		/// </summary>
+		/// <param name="authToken">The authentication token.</param>
+		void Update(string authToken);
+	}
+}
