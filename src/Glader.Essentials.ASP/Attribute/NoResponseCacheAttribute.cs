5556480baa8e9cf78ba9ccbda35c576dfaf90cab
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 00:08:15 -0600

    Added NoResponseCacheAttribute for ASP

---
+++ src/Glader.Essentials.ASP/Attribute/NoResponseCacheAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Marks an action with a <see cref="ResponseCacheAttribute"/>
+	/// with caching disables.
+	/// </summary>
+	public sealed class NoResponseCacheAttribute : ResponseCacheAttribute
+	{
+		/// <summary>
+		/// Creates a new <see cref="ResponseCacheAttribute"/> with
+		/// disables caching.
+		/// </summary>
+		public NoResponseCacheAttribute()
+		{
+			//Disables caching and storing
+			NoStore = true;
+			Location = ResponseCacheLocation.None;
+		}
+	}
+}
