6e182038986d609220fbed12fb566487c9ca39e4
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 00:07:59 -0600

    Added AuthorizeJwtAttribute for ASP

---
+++ src/Glader.Essentials.ASP/Attribute/AuthorizeJwtAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Meta-data that marks an action/controller with an <see cref="AuthorizeAttribute"/>
+	/// that indicates JWT should be used.
+	/// </summary>
+	public sealed class AuthorizeJwtAttribute : AuthorizeAttribute
+	{
+		/// <summary>
+		/// Indicates that JWT should be used.
+		/// </summary>
+		public AuthorizeJwtAttribute()
+		{
+			AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+		}
+
+		/// <summary>
+		/// Indicates that JWT should be used.
+		/// Also specifies the required Roles needed to authorize.
+		/// </summary>
+		public AuthorizeJwtAttribute(params string[] roles)
+		{
+			AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+			Roles = roles?.Aggregate("", (s, applicationRole) => $"{s},{applicationRole}");
+		}
+	}
+}
