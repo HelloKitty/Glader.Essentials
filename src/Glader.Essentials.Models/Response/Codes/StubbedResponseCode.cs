feaa34cda6cab1873c061a21e303735e8033e489
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 22:41:20 -0600

    Added StubbedResponseCode

---
+++ src/Glader.Essentials.Models/Response/Codes/StubbedResponseCode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public enum StubbedResponseCode
+	{
+		Success = ModelsCommonConstants.RESPONSE_CODE_SUCCESS_VALUE
+	}
+}
