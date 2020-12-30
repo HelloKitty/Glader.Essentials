cc93042ce4eaac44e23e3977d02bc998ea202b82
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 03:12:04 -0600

    Added IAsyncFactoryCreatable interface

---
+++ src/Glader.Essentials.Common/Factory/IAsyncFactoryCreatable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glader.Essentials
+{
+	public interface IAsyncFactoryCreatable<TCreateType, in TContextType> : IFactoryCreatable<Task<TCreateType>, TContextType>
+	{
+
+	}
+}
