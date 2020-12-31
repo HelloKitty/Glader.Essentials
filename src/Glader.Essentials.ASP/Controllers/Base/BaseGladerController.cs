8aaee44272a136f4c8b1552c2a4e234a1e076775
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 23:38:26 -0600

    Added obsolete old API for JSON responses and new Success/Failure models

--- src/Glader.Essentials.ASP/Controllers/Base/BaseGladerController.cs
+++ src/Glader.Essentials.ASP/Controllers/Base/BaseGladerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Glader.Essentials;
 using Microsoft.AspNetCore.Mvc;
@@ -31,10 +32,40 @@ namespace Glader.Essentials
 		/// <typeparam name="TModelType">The model type.</typeparam>
 		/// <param name="result">The result.</param>
 		/// <returns></returns>
-		protected JsonResult JsonSuccess<TModelType>(TModelType result)
+		[Obsolete("Use " + nameof(JsonSuccess) + " instead.")]
+		protected JsonResult BuildSuccessfulResponseModel<TModelType>(TModelType result)
 			where TModelType : class
 		{
 			if (result == null) throw new ArgumentNullException(nameof(result));
+			return Json(new ResponseModel<TModelType, StubbedResponseCode>(result)); //We use a stubbed response code since we don't actually known the result.
+		}
+
+		/// <summary>
+		/// TODO: Fix doc, intellisense not working as I'm writing this.
+		/// </summary>
+		/// <typeparam name="TResponseCodeType">The response code type.</typeparam>
+		/// <param name="failedResponseCode">The failure code.</param>
+		/// <returns></returns>
+		[Obsolete("Use " + nameof(JsonFailure) + " instead.")]
+		protected JsonResult BuildFailedResponseModel<TResponseCodeType>(TResponseCodeType failedResponseCode)
+			where TResponseCodeType : Enum
+		{
+			//TODO: add enum check.
+			return Json(new ResponseModel<object, TResponseCodeType>(failedResponseCode)); //We use a stubbed response code since we don't actually known the result.
+		}
+
+		//TODO: Fix doc
+		/// <summary>
+		/// Builds a successful JsonResult of ResponseModel
+		/// with a successful result code an the value of result.
+		/// </summary>
+		/// <typeparam name="TModelType">The model type.</typeparam>
+		/// <param name="result">The result.</param>
+		/// <returns></returns>
+		protected JsonResult JsonSuccess<TModelType>(TModelType result)
+			where TModelType : class
+		{
+			if(result == null) throw new ArgumentNullException(nameof(result));
 			return Json(new ResponseModel<TModelType, StubbedResponseCode>(result)); //We use a stubbed response code since we don't actually known the result.
 		}
 
@@ -49,6 +80,40 @@ namespace Glader.Essentials
 		{
 			//TODO: add enum check.
 			return Json(new ResponseModel<object, TResponseCodeType>(failedResponseCode)); //We use a stubbed response code since we don't actually known the result.
+		}
+
+		//TODO: Fix doc
+		/// <summary>
+		/// Builds a successful JsonResult of ResponseModel
+		/// with a successful result code an the value of result.
+		/// </summary>
+		/// <typeparam name="TModelType">The model type.</typeparam>
+		/// <typeparam name="TResponseCodeType">The response code type</typeparam>
+		/// <param name="result">The result.</param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected ResponseModel<TModelType, TResponseCodeType> Success<TModelType, TResponseCodeType>(TModelType result)
+			where TModelType : class
+			where TResponseCodeType : Enum
+		{
+			if(result == null) throw new ArgumentNullException(nameof(result));
+			return new ResponseModel<TModelType, TResponseCodeType>(result); //We use a stubbed response code since we don't actually known the result.
+		}
+
+		/// <summary>
+		/// TODO: Fix doc, intellisense not working as I'm writing this.
+		/// </summary>
+		/// <typeparam name="TResponseCodeType">The response code type.</typeparam>
+		/// <typeparam name="TModelType">The response model.</typeparam>
+		/// <param name="failedResponseCode">The failure code.</param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected ResponseModel<TModelType, TResponseCodeType> Failure<TModelType, TResponseCodeType>(TResponseCodeType failedResponseCode)
+			where TResponseCodeType : Enum
+			where TModelType : class
+		{
+			//TODO: add enum check.
+			return new ResponseModel<TModelType, TResponseCodeType>(failedResponseCode); //We use a stubbed response code since we don't actually known the result.
 		}
 	}
 }
