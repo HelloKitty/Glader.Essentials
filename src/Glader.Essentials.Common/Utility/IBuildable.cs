67aeed847493181e475bc2bd1c1df066bdca1d30
HelloKitty <andrew.blakely@ymail.com>
2021-01-05 06:57:57 -0600

    Added IBuildable to Common lib

---
+++ src/Glader.Essentials.Common/Utility/IBuildable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for types that can build an instance of
+	/// <typeparamref name="T"/>.
+	/// </summary>
+	/// <typeparam name="T">The buildable type.</typeparam>
+	public interface IBuildable<out T>
+	{
+		/// <summary>
+		/// Builds an instance of the type.
+		/// </summary>
+		/// <returns>Returns the instance built.</returns>
+		T Build();
+	}
+}
