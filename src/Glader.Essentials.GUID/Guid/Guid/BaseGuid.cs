a691a9eb0cbc558f5baaffe78f2b20f8e4ddcde6
HelloKitty <andrew.blakely@ymail.com>
2021-01-05 06:53:31 -0600

    Added initial Essentials.GUID cspoj

---
+++ src/Glader.Essentials.GUID/Guid/Guid/BaseGuid.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Base non-generic guid type.
+	/// </summary>
+	public abstract class BaseGuid : IEquatable<BaseGuid>
+	{
+		/// <summary>
+		/// Internal GUID value.
+		/// </summary>
+		[DataMember(Order = 1, IsRequired = true)]
+		[IgnoreDataMember]
+		public ulong RawGuidValue { get; internal set; } //setter only for serialization
+
+		/// <summary>
+		/// Indicates the current GUID of the object. This is the last chunk represents the id that the world server assigned to the object. (The rest is just maskable flags about the object)
+		/// </summary>
+		[IgnoreDataMember]
+		public int CurrentObjectGuid => (int)(RawGuidValue & 0x0000000000FFFFFF);
+
+		/// <summary>
+		/// Indicates the templated reference identifier (entry)
+		/// for the Entity.
+		/// </summary>
+		[IgnoreDataMember]
+		public int Entry => (int) (RawGuidValue >> 24) & 0x0000000000FFFFFF;
+
+		//TODO: This only support 15 shards.
+		/// <summary>
+		/// Represents the unique bits related to shard/server identifier.
+		/// Meaning an object attached to one shard may be globally unique against one, similar or identical,
+		/// to another except for these top-most bits.
+		/// </summary>
+		[IgnoreDataMember]
+		public byte ShardIdentifier => (byte) ((RawGuidValue >> 63) & 0xF);
+
+		/// <summary>
+		/// Indicates if the entity has an entry.
+		/// </summary>
+		[IgnoreDataMember]
+		public bool HasEntry => Entry != 0;
+
+		/// <summary>
+		/// Indicates if the GUID is an empty or uninitialized GUID.
+		/// </summary>
+		/// <returns></returns>
+		public bool IsEmpty()
+		{
+			return RawGuidValue == 0;
+		}
+
+		/// <inheritdoc />
+		public bool Equals(BaseGuid other)
+		{
+			if (other == null)
+				return false;
+			else
+				return other.RawGuidValue == this.RawGuidValue;
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((BaseGuid) obj);
+		}
+
+		public static bool operator ==(BaseGuid lhs, BaseGuid rhs)
+		{
+			// Check for null on left side.
+			if(Object.ReferenceEquals(lhs, null))
+			{
+				if(Object.ReferenceEquals(rhs, null))
+				{
+					// null == null = true.
+					return true;
+				}
+
+				// Only the left side is null.
+				return false;
+			}
+
+
+			// Equals handles case of null on right side.
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(BaseGuid lhs, BaseGuid rhs)
+		{
+			return !(lhs == rhs);
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			//We actually ONLY mutate it during deserialization.
+			// ReSharper disable once NonReadonlyMemberInGetHashCode
+			return RawGuidValue.GetHashCode();
+		}
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return $"{RawGuidValue} 0x{RawGuidValue:X} Entry: {Entry} Id: {CurrentObjectGuid}";
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected BaseGuid()
+		{
+
+		}
+	}
+}
---
+++ src/Glader.Essentials.GUID/Guid/Guid/ObjectGuid.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// <see cref="BaseGuid"/> implementation that defines a specific <typeparamref name="TEntityType"/>.
+	/// </summary>
+	/// <typeparam name="TEntityType"></typeparam>
+	public class ObjectGuid<TEntityType> : BaseGuid, IEquatable<ObjectGuid<TEntityType>>
+		where TEntityType : Enum
+	{
+		/// <summary>
+		/// Represents an Empty or uninitialized <see cref="ObjectGuid{TEntityType}"/>.
+		/// </summary>
+		[IgnoreDataMember]
+		public static ObjectGuid<TEntityType> Empty { get; } = new ObjectGuid<TEntityType>(0);
+
+		//DO NOT REMOVE!
+		static ObjectGuid()
+		{
+			
+		}
+
+		/// <summary>
+		/// Indicates the object Type that the <see cref="ObjectGuid{TEntityType}"/> is associated with.
+		/// </summary>
+		[IgnoreDataMember]
+		public TEntityType ObjectType => CalculateObjectType();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private TEntityType CalculateObjectType()
+		{
+			//WoW would be 0x0000FFFF but we trim this down abit.
+			ulong value = (RawGuidValue >> 48) & 0x00000FFF;
+			return Unsafe.As<ulong, TEntityType>(ref value);
+		}
+
+		/// <summary>
+		/// Indicates if the Entity is of the provided object type.
+		/// </summary>
+		/// <param name="objectType">The object type to check.</param>
+		/// <returns>True if they're matching object types.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool IsType(TEntityType objectType)
+		{
+			return EqualityComparer<TEntityType>.Default.Equals(ObjectType, objectType);
+		}
+
+		/// <summary>
+		/// Creates a new value-type wrapped for the uint64 raw GUID value.
+		/// </summary>
+		/// <param name="guidValue">Raw GUID value.</param>
+		public ObjectGuid(ulong guidValue)
+		{
+			RawGuidValue = guidValue;
+		}
+
+		public ObjectGuid()
+			: this(0)
+		{
+
+		}
+
+		/// <summary>
+		/// Implict cast to ulong (uint64 TC/C++)
+		/// </summary>
+		/// <param name="guid"></param>
+		public static implicit operator ulong(ObjectGuid<TEntityType> guid)
+		{
+			return guid.RawGuidValue;
+		}
+
+		/// <summary>
+		/// Implict cast to ulong (uint64 TC/C++)
+		/// </summary>
+		/// <param name="guid"></param>
+		public static implicit operator ObjectGuid<TEntityType>(ulong guid)
+		{
+			if (guid == 0)
+				return Empty;
+
+			return new ObjectGuid<TEntityType>(guid);
+		}
+
+		public bool Equals(ObjectGuid<TEntityType> other)
+		{
+			if(ReferenceEquals(null, other)) return false;
+			if(ReferenceEquals(this, other)) return true;
+			return base.Equals(other) && RawGuidValue == other.RawGuidValue;
+		}
+
+		public static bool operator ==(ObjectGuid<TEntityType> lhs, ObjectGuid<TEntityType> rhs)
+		{
+			// Check for null on left side.
+			if(Object.ReferenceEquals(lhs, null))
+			{
+				if(Object.ReferenceEquals(rhs, null))
+				{
+					// null == null = true.
+					return true;
+				}
+
+				// Only the left side is null.
+				return false;
+			}
+
+
+			// Equals handles case of null on right side.
+			return lhs.Equals(rhs);
+		}
+
+		public static bool operator !=(ObjectGuid<TEntityType> lhs, ObjectGuid<TEntityType> rhs)
+		{
+			return !(lhs == rhs);
+		}
+
+		public sealed override string ToString()
+		{
+			return $"Type: {ObjectType} {base.ToString()}";
+		}
+
+		public sealed override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
+
+		public sealed override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+}
