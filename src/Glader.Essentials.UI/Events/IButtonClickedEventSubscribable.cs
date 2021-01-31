7aa2503348d55c301b1689f9a719b9754bf502a6
HelloKitty <andrew.blakely@ymail.com>
2021-01-31 00:20:43 -0600

    Added IButtonClickedEventSubscribable

---
+++ src/Glader.Essentials.UI/Events/IButtonClickedEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event subscription interface for button events.
+	/// </summary>
+	public interface IButtonClickedEventSubscribable
+	{
+		event EventHandler<ButtonClickedEventArgs> OnButtonClicked;
+	}
+
+	/// <summary>
+	/// <see cref="EventArgs"/> for the button click event.
+	/// </summary>
+	public sealed class ButtonClickedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The button being clicked.
+		/// </summary>
+		public IUIButton Button { get; }
+
+		public ButtonClickedEventArgs(IUIButton button)
+		{
+			Button = button ?? throw new ArgumentNullException(nameof(button));
+		}
+	}
+}
