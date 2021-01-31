d3c00d0952f04347dbab8358d3e356c02d9e5fce
HelloKitty <andrew.blakely@ymail.com>
2021-01-30 21:34:07 -0600

    Added ButtonAttribute

---
+++ src/Glader.Essentials.Unity/Attributes/ButtonAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Unity3D meta-data attribute that indicates the function should be a clickable button
+	/// in the Unity3D Editor.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public sealed class ButtonAttribute : Attribute
+	{
+
+	}
+}
