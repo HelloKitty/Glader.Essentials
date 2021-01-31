a2c0dc009cec1f127183931f7eed50397c47047e
HelloKitty <andrew.blakely@ymail.com>
2021-01-30 21:34:14 -0600

    Added GladerBehaviour

---
+++ src/Glader.Essentials.Unity/MonoBehaviour/GladerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Base-behaviour used for Unity3D <see cref="MonoBehaviour"/> in projects that
+	/// use Glader.Essentials
+	/// </summary>
+	public abstract class GladerBehaviour : MonoBehaviour
+	{
+
+	}
+}
