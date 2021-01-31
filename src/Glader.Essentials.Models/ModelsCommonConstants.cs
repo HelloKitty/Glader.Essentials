63b3215347a311d2f70211392a94e66b81be1bde
HelloKitty <andrew.blakely@ymail.com>
2021-01-30 19:39:28 -0600

    Obsolute ModelsCommonConstants

--- src/Glader.Essentials.Models/ModelsCommonConstants.cs
+++ src/Glader.Essentials.Models/ModelsCommonConstants.cs
@@ -4,11 +4,13 @@ using System.Text;
 
 namespace Glader.Essentials
 {
+	[Obsolete]
 	public static class ModelsCommonConstants
 	{
 		/// <summary>
 		/// This is the constant/known integer value of a successful response code.
 		/// </summary>
+		[Obsolete]
 		public const int RESPONSE_CODE_SUCCESS_VALUE = 1;
 	}
 }
