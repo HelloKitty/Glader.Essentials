c798b72e94b63fe0f363ee2a7b5fc36578b73e9c
HelloKitty <andrew.blakely@ymail.com>
2021-02-02 04:07:43 -0600

    Added ButtonClickedEventListener

--- src/Glader.Essentials.UI/Events/IButtonClickedEventSubscribable.cs
+++ src/Glader.Essentials.UI/Events/IButtonClickedEventSubscribable.cs
@@ -9,6 +9,9 @@ namespace Glader.Essentials
 	/// </summary>
 	public interface IButtonClickedEventSubscribable
 	{
+		/// <summary>
+		/// Event fired when the button is clicked.
+		/// </summary>
 		event EventHandler<ButtonClickedEventArgs> OnButtonClicked;
 	}
 
@@ -22,9 +25,40 @@ namespace Glader.Essentials
 		/// </summary>
 		public IUIButton Button { get; }
 
+		/// <inheritdoc />
 		public ButtonClickedEventArgs(IUIButton button)
 		{
 			Button = button ?? throw new ArgumentNullException(nameof(button));
+		}
+	}
+
+	/// <summary>
+	/// Base event listener that can listen for named/typed button events.
+	/// </summary>
+	/// <typeparam name="TButtonClickedEventType">The button event type.</typeparam>
+	public abstract class ButtonClickedEventListener<TButtonClickedEventType> : EngineEventListener<TButtonClickedEventType, ButtonClickedEventArgs>
+		where TButtonClickedEventType : class, IButtonClickedEventSubscribable
+	{
+		/// <inheritdoc />
+		protected ButtonClickedEventListener(TButtonClickedEventType subscriptionService)
+			: base(subscriptionService)
+		{
+
+		}
+	}
+
+	/// <summary>
+	/// Base event listener that can listen for named/typed button events and dispatch their click handling async.
+	/// </summary>
+	/// <typeparam name="TButtonClickedEventType">The button event type.</typeparam>
+	public abstract class ButtonClickedEventListenerAsync<TButtonClickedEventType> : EngineEventListenerAsync<TButtonClickedEventType, ButtonClickedEventArgs>
+		where TButtonClickedEventType : class, IButtonClickedEventSubscribable
+	{
+		/// <inheritdoc />
+		protected ButtonClickedEventListenerAsync(TButtonClickedEventType subscriptionService)
+			: base(subscriptionService)
+		{
+
 		}
 	}
 }
