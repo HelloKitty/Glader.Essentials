63fbe1e1ffc0c9e939d4dadfa34c18e86541e483
HelloKitty <andrew.blakely@ymail.com>
2021-03-17 12:31:25 -0500

    Added IOperationCodeable

---
+++ src/Glader.Essentials.Models/Interfaces/IOperationCodeable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for types that can be mapped to an <typeparamref name="TOperationCodeType"/> code.
+	/// </summary>
+	/// <typeparam name="TOperationCodeType">The operation code type.</typeparam>
+	public interface IOperationCodeable<out TOperationCodeType>
+		where TOperationCodeType : Enum
+	{
+		/// <summary>
+		/// The operation code.
+		/// </summary>
+		TOperationCodeType OperationCode { get; }
+	}
+}
