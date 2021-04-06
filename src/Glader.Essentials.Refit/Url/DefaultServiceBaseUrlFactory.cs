84302036ce2418d2364c35bf824d8a4f24d62310
HelloKitty <andrew.blakely@ymail.com>
2021-04-06 01:36:48 -0500

    Added IServiceBaseUrlFactory from Booma project

---
+++ src/Glader.Essentials.Refit/Url/DefaultServiceBaseUrlFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Booma
+{
+	/// <summary>
+	/// Default simple implementation of <see cref="IServiceBaseUrlFactory"/>.
+	/// </summary>
+	public sealed class DefaultServiceBaseUrlFactory : IServiceBaseUrlFactory
+	{
+		/// <inheritdoc />
+		public string Create(Uri context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			return context.GetLeftPart(UriPartial.Authority);
+		}
+	}
+}
---
+++ src/Glader.Essentials.Refit/Url/IServiceBaseUrlFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for types that can build a <see cref="string"/> service path from an input <see cref="Uri"/>.
+	/// </summary>
+	public interface IServiceBaseUrlFactory : IFactoryCreatable<string, Uri>
+	{
+
+	}
+}
