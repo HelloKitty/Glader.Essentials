1c23bd53d72550f0ab116ba812d1219e047241b7
HelloKitty <andrew.blakely@ymail.com>
2021-04-06 01:37:13 -0500

    Fixed Booma namespace

--- src/Glader.Essentials.Refit/Url/DefaultServiceBaseUrlFactory.cs
+++ src/Glader.Essentials.Refit/Url/DefaultServiceBaseUrlFactory.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Glader.Essentials;
 
-namespace Booma
+namespace Glader.Essentials
 {
 	/// <summary>
 	/// Default simple implementation of <see cref="IServiceBaseUrlFactory"/>.
