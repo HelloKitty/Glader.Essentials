67f770e409850a98989d2bd0ac8702114fed5f6b
HelloKitty <andrew.blakely@ymail.com>
2021-04-21 15:55:10 -0500

    Added GetSubAccountId generic extension for claims reader

--- src/Glader.Essentials.ASP/Extensions/IClaimsPrincipalReaderExtensions.cs
+++ src/Glader.Essentials.ASP/Extensions/IClaimsPrincipalReaderExtensions.cs
@@ -9,19 +9,32 @@ namespace Glader.Essentials
 	public static class IClaimsPrincipalReaderExtensions
 	{
 		/// <summary>
-		/// Returns the User ID claim value if present otherwise will throw.
+		/// Returns the User's Sub-Account ID claim value if present otherwise will throw.
 		/// </summary>
 		/// <param name="reader"></param>
 		/// <param name="principal">The <see cref="ClaimsPrincipal"/> instance.</param>
 		/// <returns>The User ID claim value. Will throw if the principal doesn't contain the id.</returns>
 		/// <exception cref="ArgumentException">Throws if the provided principal doesn't contain an id.</exception>
 		/// <remarks>The User ID claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
-		public static int GetAccountId(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+		public static TIdentifierType GetSubAccountId<TIdentifierType>(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+			where TIdentifierType : unmanaged
 		{
-			if(!int.TryParse(reader.GetAccountId(principal), out var accountId))
-				throw new ArgumentException($"Provided {nameof(ClaimsPrincipal)} does not contain a user id.", nameof(principal));
+			try
+			{
+				string id = reader.GetSubAccountId(principal);
 
-			return accountId;
+				if(typeof(TIdentifierType) == typeof(string))
+					return (TIdentifierType)(object)id;
+
+				if(id == default)
+					return default;
+
+				return (TIdentifierType)Convert.ChangeType(id, typeof(TIdentifierType));
+			}
+			catch(Exception e)
+			{
+				throw new ArgumentException($"Provided {nameof(ClaimsPrincipal)} does not contain a user id.", nameof(principal), e);
+			}
 		}
 
 		/// <summary>
@@ -33,6 +46,7 @@ namespace Glader.Essentials
 		/// <exception cref="ArgumentException">Throws if the provided principal doesn't contain an id.</exception>
 		/// <remarks>The User ID claim is identified by <see cref="ClaimTypes.NameIdentifier"/>.</remarks>
 		public static TIdentifierType GetAccountId<TIdentifierType>(this IClaimsPrincipalReader reader, ClaimsPrincipal principal)
+			where TIdentifierType : unmanaged
 		{
 			try
 			{
