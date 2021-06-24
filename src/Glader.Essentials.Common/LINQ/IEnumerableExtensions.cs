d60740ceb2c8b95c1bd05bede00ba40dd61fa084
HelloKitty <andrew.blakely@ymail.com>
2021-06-23 22:10:38 -0500

    Added IEnumerable Extension ForEach

---
+++ src/Glader.Essentials.Common/LINQ/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public static class IEnumerableExtensions
+	{
+		/// <summary>
+		/// LINQ-like implementation of <see cref="List{T}"/>'s ForEach method.
+		/// Iterates the provided <see cref="enumerable"/> and calls the provided <see cref="action"/>
+		/// </summary>
+		/// <typeparam name="T">Element type.</typeparam>
+		/// <param name="enumerable">Enumerable.</param>
+		/// <param name="action">Action to call for each element.</param>
+		/// <returns>The enumerable.</returns>
+		public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+		{
+			if (action == null) throw new ArgumentNullException(nameof(action));
+
+			if (enumerable == null)
+				return null;
+
+			foreach (var t in enumerable)
+				action(t);
+
+			return enumerable;
+		}
+	}
+}
