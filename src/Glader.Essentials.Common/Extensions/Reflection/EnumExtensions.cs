3a5a28f8cfe75292c717b73bcecede211ca06a7e
HelloKitty <andrew.blakely@ymail.com>
2021-07-09 15:20:11 -0500

    Added Enum attribute extensions

---
+++ src/Glader.Essentials.Common/Extensions/Reflection/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public static class EnumExtensions
+	{
+		/// <summary>
+		/// Retrieves the attribute from the enum field value.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>The attribute on the enum field value if it exists.</returns>
+		public static TAttributeType GetEnumFieldAttribute<TAttributeType>(this Enum value)
+			where TAttributeType : Attribute
+		{
+			return value.GetType()
+				.GetField(value.ToString())
+				?.GetCustomAttribute<TAttributeType>(false);
+		}
+	}
+}
