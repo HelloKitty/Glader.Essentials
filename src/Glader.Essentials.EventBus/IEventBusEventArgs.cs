2155a60f2337bcf4fe8dd3078e89cf4887a499ba
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 06:00:03 -0500

    Added IEventBusEventArgs

---
+++ src/Glader.Essentials.EventBus/IEventBusEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Marker interface for event bus event args.
+	/// Can be thought like <see cref="EventArgs"/> from traditional event systems.
+	/// </summary>
+	public interface IEventBusEventArgs
+	{
+
+	}
+}
