961a27226ff1112b680f600ad70ff406e1733974
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 08:03:19 -0500

    Added some basic Tests From RedBus

---
+++ src/Glader.Essentials.EventBus/BasicEventBusEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Generic <see cref="IEventBusEventArgs"/> that can adapt a non-<see cref="IEventBusEventArgs"/> object as the event.
+	/// </summary>
+	public class BasicEventBusEventArgs<TPayload> : IEventBusEventArgs
+	{
+		/// <summary>
+		/// The adapted payload for the event.
+		/// </summary>
+		public TPayload Payload { get; }
+
+		public BasicEventBusEventArgs(TPayload payload)
+		{
+			Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+		}
+	}
+}
