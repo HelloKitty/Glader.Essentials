450c56b57187b48f8ee182d9b9f1fede5dd5035d
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 08:56:44 -0500

    Added SubUnsub perf test

--- tests/Glader.Essentials.EventBus.Tests/PerfTests.cs
+++ tests/Glader.Essentials.EventBus.Tests/PerfTests.cs
@@ -8,72 +8,114 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Redbus.Tests
 {
-    [TestClass]
-    public class PerfTests
-    {
-        // Naive baseline to see changes for now
-        [TestMethod]
-        public void NaivePublishPerformanceTest()
-        {
-            var eventBus = new EventBus();
-            eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
+	[TestClass]
+	public class PerfTests
+	{
+		// Naive baseline to see changes for now
+		[TestMethod]
+		public void NaivePublishPerformanceTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
 
-            GC.TryStartNoGCRegion(int.MaxValue, true);
-            var sw = Stopwatch.StartNew();
-            for(int i = 0; i < 10000000; i++)
-                eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
-            sw.Stop();
-            GC.EndNoGCRegion();
+			GC.TryStartNoGCRegion(int.MaxValue, true);
+			var sw = Stopwatch.StartNew();
+			for(int i = 0; i < 10000000; i++)
+				eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
+			sw.Stop();
+			GC.EndNoGCRegion();
 
-            Debug.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
-            Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(1), $"NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
-            Console.WriteLine($"[DEBUG] NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
-        }
+			Debug.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
+			Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(1), $"NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
+			Console.WriteLine($"[DEBUG] NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
+		}
 
-        [TestMethod]
-        public void NaiveConcurrentPublishPerformanceTest()
-        {
-            var eventBus = new EventBus();
-            eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
+		[TestMethod]
+		public void NaiveConcurrentPublishPerformanceTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
 
-            var mainSw = Stopwatch.StartNew();
-            var threads = new List<Thread>();
-            for(int i = 0; i < 64; i++)
-            {
-                var thread = new Thread(() =>
-                {
-                    var sw = Stopwatch.StartNew();
-                    for(int j = 0; j < 1000000; j++)
-                    {
-                        eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
-                    }
-                    sw.Stop();
-                    Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(1), $"NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
-                });
+			var mainSw = Stopwatch.StartNew();
+			var threads = new List<Thread>();
+			for(int i = 0; i < 64; i++)
+			{
+				var thread = new Thread(() =>
+				{
+					var sw = Stopwatch.StartNew();
+					for(int j = 0; j < 1000000; j++)
+					{
+						eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
+					}
+					sw.Stop();
+					Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(1), $"NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
+				});
 
-                threads.Add(thread);
-            }
+				threads.Add(thread);
+			}
 
-            foreach(var thread in threads)
-            {
-                thread.Start();
-            }
+			foreach(var thread in threads)
+			{
+				thread.Start();
+			}
 
-            foreach(var thread in threads)
-            {
-                thread.Join();
-            }
-            mainSw.Stop();
+			foreach(var thread in threads)
+			{
+				thread.Join();
+			}
+			mainSw.Stop();
 
-            Debug.WriteLine($"Finished in {mainSw.ElapsedMilliseconds}ms");
-            Assert.IsTrue(mainSw.Elapsed < TimeSpan.FromSeconds(2), $"NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
-            Console.WriteLine($"[DEBUG] NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
-        }
+			Debug.WriteLine($"Finished in {mainSw.ElapsedMilliseconds}ms");
+			Assert.IsTrue(mainSw.Elapsed < TimeSpan.FromSeconds(2), $"NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
+			Console.WriteLine($"[DEBUG] NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
+		}
 
-        [MethodImpl(MethodImplOptions.NoOptimization)]
-        private void CustomTestEventHandler(object sender, CustomTestEvent customTestEvent)
-        {
+		[TestMethod]
+		public void NaiveConcurrentSubUnsubPerformanceTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
 
-        }
-    }
+			var mainSw = Stopwatch.StartNew();
+			var threads = new List<Thread>();
+			for(int i = 0; i < 64; i++)
+			{
+				var thread = new Thread(() =>
+				{
+					var sw = Stopwatch.StartNew();
+					for(int j = 0; j < 10000; j++)
+					{
+						var token = eventBus.Subscribe<CustomTestEvent>(CustomTestEventHandler);
+						eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
+						eventBus.Unsubscribe(token);
+					}
+					sw.Stop();
+					Assert.IsTrue(sw.Elapsed < TimeSpan.FromSeconds(1), $"NaivePublishPerformanceTest took {sw.ElapsedMilliseconds}ms");
+				});
+
+				threads.Add(thread);
+			}
+
+			foreach(var thread in threads)
+			{
+				thread.Start();
+			}
+
+			foreach(var thread in threads)
+			{
+				thread.Join();
+			}
+			mainSw.Stop();
+
+			Debug.WriteLine($"Finished in {mainSw.ElapsedMilliseconds}ms");
+			Assert.IsTrue(mainSw.Elapsed < TimeSpan.FromSeconds(2), $"NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
+			Console.WriteLine($"[DEBUG] NaivePublishPerformanceTest took {mainSw.ElapsedMilliseconds}ms");
+		}
+
+		[MethodImpl(MethodImplOptions.NoOptimization)]
+		private void CustomTestEventHandler(object sender, CustomTestEvent customTestEvent)
+		{
+
+		}
+	}
 }
