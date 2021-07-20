fe58b93913431494afae48dd6eaa35603f77459a
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 05:59:53 -0500

    Added EventBusLock

---
+++ src/Glader.Essentials.EventBus/EventBusLock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Glader.Essentials
+{
+	internal class EventBusLock<TEventType>
+		where TEventType : IEventBusEventArgs
+	{
+		/// <summary>
+		/// Eventbus lock object for the specified <typeparamref name="TEventType"/>.
+		/// </summary>
+		public static ReaderWriterLockSlim Lock { get; } = new ReaderWriterLockSlim();
+
+		//WARNING: DO NOT REMOTE!
+		static EventBusLock()
+		{
+
+		}
+	}
+}
