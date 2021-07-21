51150d27696b80d33277b84922023c49c8db4b2f
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 21:53:12 -0500

    Fixed EventBusLock to support multiple EventBus in the same AppDomains

--- src/Glader.Essentials.EventBus/EventBusLock.cs
+++ src/Glader.Essentials.EventBus/EventBusLock.cs
@@ -1,22 +1,33 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
 namespace Glader.Essentials
 {
-	internal class EventBusLock<TEventType>
-		where TEventType : IEventBusEventArgs
+	internal class EventBusLock
 	{
-		/// <summary>
-		/// Eventbus lock object for the specified <typeparamref name="TEventType"/>.
-		/// </summary>
-		public static ReaderWriterLockSlim Lock { get; } = new ReaderWriterLockSlim();
+		private object SyncObj { get; } = new object();
 
-		//WARNING: DO NOT REMOTE!
-		static EventBusLock()
+		private ConcurrentDictionary<Type, ReaderWriterLockSlim> LockMap { get; } = new();
+
+		public ReaderWriterLockSlim GetLock<TEventType>()
+			where TEventType : IEventBusEventArgs
 		{
+			//We can try to load it directly without locking but we must lock if we
+			//want to create a new one
+			if (LockMap.TryGetValue(typeof(TEventType), out var lockValue))
+				return lockValue;
 
+			lock (SyncObj)
+			{
+				//Double check lock
+				if (LockMap.TryGetValue(typeof(TEventType), out lockValue))
+					return lockValue;
+
+				return LockMap[typeof(TEventType)] = new ReaderWriterLockSlim();
+			}
 		}
 	}
 }
