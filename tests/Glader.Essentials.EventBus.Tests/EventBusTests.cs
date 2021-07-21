b9d4f15da907a676f791c1f638c3c02f0cfd7c1b
HelloKitty <andrew.blakely@ymail.com>
2021-07-20 23:03:40 -0500

    Tabify EventBusTests

--- tests/Glader.Essentials.EventBus.Tests/EventBusTests.cs
+++ tests/Glader.Essentials.EventBus.Tests/EventBusTests.cs
@@ -5,144 +5,144 @@ using System.Threading;
 
 namespace Glader.Essentials
 {
-    //Based on: https://github.com/mxjones/RedBus/blob/master/src/Redbus/Redbus.Tests/EventBusTests.cs
-    [TestClass]
-    public class EventBusTests
-    {
-        private bool _methodHandlerHit;
-        private bool _actionHandlerHit;
+	//Based on: https://github.com/mxjones/RedBus/blob/master/src/Redbus/Redbus.Tests/EventBusTests.cs
+	[TestClass]
+	public class EventBusTests
+	{
+		private bool _methodHandlerHit;
+		private bool _actionHandlerHit;
 
-        [TestInitialize]
-        public void Initialize()
-        {
-            _methodHandlerHit = false;
-            _actionHandlerHit = false;
-        }
+		[TestInitialize]
+		public void Initialize()
+		{
+			_methodHandlerHit = false;
+			_actionHandlerHit = false;
+		}
 
-        [TestMethod]
-        public void SubscribeAndPublishCustomEventMethodTest()
-        {
-            var eventBus = new EventBus();
-            eventBus.Subscribe<CustomTestEvent>(CustomTestEventMethodHandler);
+		[TestMethod]
+		public void SubscribeAndPublishCustomEventMethodTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<CustomTestEvent>(CustomTestEventMethodHandler);
 
-            Assert.IsFalse(_methodHandlerHit);
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
-            Assert.IsTrue(_methodHandlerHit);
-        }
+			Assert.IsFalse(_methodHandlerHit);
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event", Identifier = 1 });
+			Assert.IsTrue(_methodHandlerHit);
+		}
 
-        [TestMethod]
-        public void SubscribeAndPublishCustomEventActionTest()
-        {
-            var eventBus = new EventBus();
-            eventBus.Subscribe<CustomTestEvent>((obj, s) =>
-            {
-                Assert.AreEqual("Custom Event 2", s.Name);
-                Assert.AreEqual(2, s.Identifier);
+		[TestMethod]
+		public void SubscribeAndPublishCustomEventActionTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<CustomTestEvent>((obj, s) =>
+			{
+				Assert.AreEqual("Custom Event 2", s.Name);
+				Assert.AreEqual(2, s.Identifier);
 
-                _actionHandlerHit = true;
-            });
+				_actionHandlerHit = true;
+			});
 
-            Assert.IsFalse(_actionHandlerHit);
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event 2", Identifier = 2 });
-            Assert.IsTrue(_actionHandlerHit);
-        }
+			Assert.IsFalse(_actionHandlerHit);
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event 2", Identifier = 2 });
+			Assert.IsTrue(_actionHandlerHit);
+		}
 
-        [TestMethod]
-        public void SubscribeAndPublishBuiltInEventActionTest()
-        {
-            var eventBus = new EventBus();
-            eventBus.Subscribe<BasicEventBusEventArgs<int>>((obj, s) =>
-            {
-                Assert.AreEqual(999, s.Payload);
-                _actionHandlerHit = true;
-            });
+		[TestMethod]
+		public void SubscribeAndPublishBuiltInEventActionTest()
+		{
+			var eventBus = new EventBus();
+			eventBus.Subscribe<BasicEventBusEventArgs<int>>((obj, s) =>
+			{
+				Assert.AreEqual(999, s.Payload);
+				_actionHandlerHit = true;
+			});
 
-            Assert.IsFalse(_actionHandlerHit);
-            eventBus.Publish(this, new BasicEventBusEventArgs<int>(999));
-            Assert.IsTrue(_actionHandlerHit);
-        }
+			Assert.IsFalse(_actionHandlerHit);
+			eventBus.Publish(this, new BasicEventBusEventArgs<int>(999));
+			Assert.IsTrue(_actionHandlerHit);
+		}
 
-        [TestMethod]
-        public void PublishInCorrectOrderTest()
-        {
-            var eventBus = new EventBus();
+		[TestMethod]
+		public void PublishInCorrectOrderTest()
+		{
+			var eventBus = new EventBus();
 
-            List<CustomTestEvent> customTestEventResults = new List<CustomTestEvent>();
-            eventBus.Subscribe<CustomTestEvent>((obj, s) =>
-            {
-                customTestEventResults.Add(s);
-            });
+			List<CustomTestEvent> customTestEventResults = new List<CustomTestEvent>();
+			eventBus.Subscribe<CustomTestEvent>((obj, s) =>
+			{
+				customTestEventResults.Add(s);
+			});
 
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 1 });
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 2 });
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 3 });
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 4 });
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 5 });
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 6 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 1 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 2 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 3 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 4 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 5 });
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Test Event", Identifier = 6 });
 
-            Assert.AreEqual(6, customTestEventResults.Count);
-            Assert.AreEqual(1, customTestEventResults[0].Identifier);
-            Assert.AreEqual(2, customTestEventResults[1].Identifier);
-            Assert.AreEqual(3, customTestEventResults[2].Identifier);
-            Assert.AreEqual(4, customTestEventResults[3].Identifier);
-            Assert.AreEqual(5, customTestEventResults[4].Identifier);
-            Assert.AreEqual(6, customTestEventResults[5].Identifier);
-        }
+			Assert.AreEqual(6, customTestEventResults.Count);
+			Assert.AreEqual(1, customTestEventResults[0].Identifier);
+			Assert.AreEqual(2, customTestEventResults[1].Identifier);
+			Assert.AreEqual(3, customTestEventResults[2].Identifier);
+			Assert.AreEqual(4, customTestEventResults[3].Identifier);
+			Assert.AreEqual(5, customTestEventResults[4].Identifier);
+			Assert.AreEqual(6, customTestEventResults[5].Identifier);
+		}
 
-        [TestMethod]
-        public void UnsubscribeTest()
-        {
-            var eventBus = new EventBus();
-            var token = eventBus.Subscribe<CustomTestEvent>((obj, s) =>
-            {
-                Assert.Fail("This should not be executed due to unsubscribing.");
-            });
+		[TestMethod]
+		public void UnsubscribeTest()
+		{
+			var eventBus = new EventBus();
+			var token = eventBus.Subscribe<CustomTestEvent>((obj, s) =>
+			{
+				Assert.Fail("This should not be executed due to unsubscribing.");
+			});
 
-            eventBus.Unsubscribe(token);
-            eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event 3", Identifier = 3 });
-        }
+			eventBus.Unsubscribe(token);
+			eventBus.Publish(this, new CustomTestEvent { Name = "Custom Event 3", Identifier = 3 });
+		}
 
-        [TestMethod]
-        public void UnsubscribeDontThrowIfDoesntExistTest()
-        {
-            var eventBus = new EventBus();
-            var token = eventBus.Subscribe<CustomTestEvent>((obj, s) =>
-            {
-                Assert.Fail("This should not be executed due to unsubscribing.");
-            });
+		[TestMethod]
+		public void UnsubscribeDontThrowIfDoesntExistTest()
+		{
+			var eventBus = new EventBus();
+			var token = eventBus.Subscribe<CustomTestEvent>((obj, s) =>
+			{
+				Assert.Fail("This should not be executed due to unsubscribing.");
+			});
 
-            eventBus.Unsubscribe(token);
-            eventBus.Unsubscribe(token);
-            eventBus.Unsubscribe(token);
-            eventBus.Unsubscribe(token);
-        }
+			eventBus.Unsubscribe(token);
+			eventBus.Unsubscribe(token);
+			eventBus.Unsubscribe(token);
+			eventBus.Unsubscribe(token);
+		}
 
-        [TestMethod]
-        public void PublishThrowSubscriberExceptionTest()
-        {
-            var eventBus = new EventBus();
-            bool firstSubscriberHit = false, thirdSubscriberHit = false;
-            eventBus.Subscribe<CustomTestEvent>((obj, s) => { firstSubscriberHit = true; });
-            eventBus.Subscribe<CustomTestEvent>((obj, s) => throw new ApplicationException($"Subscriber error"));
-            eventBus.Subscribe<CustomTestEvent>((obj, s) => { thirdSubscriberHit = true; });
+		[TestMethod]
+		public void PublishThrowSubscriberExceptionTest()
+		{
+			var eventBus = new EventBus();
+			bool firstSubscriberHit = false, thirdSubscriberHit = false;
+			eventBus.Subscribe<CustomTestEvent>((obj, s) => { firstSubscriberHit = true; });
+			eventBus.Subscribe<CustomTestEvent>((obj, s) => throw new ApplicationException($"Subscriber error"));
+			eventBus.Subscribe<CustomTestEvent>((obj, s) => { thirdSubscriberHit = true; });
 
-            var thrownException = Assert.ThrowsException<ApplicationException>(() => eventBus.Publish(this, new CustomTestEvent())); // Subscriber exception is thrown
-            Assert.AreEqual("Subscriber error", thrownException.Message); // Verify correct message from subscriber
-            Assert.IsTrue(firstSubscriberHit); // The first subscriber will be hit
-            Assert.IsFalse(thirdSubscriberHit); // Third subscriber will not be hit, missed due to thrown exception.
-        }
+			var thrownException = Assert.ThrowsException<ApplicationException>(() => eventBus.Publish(this, new CustomTestEvent())); // Subscriber exception is thrown
+			Assert.AreEqual("Subscriber error", thrownException.Message); // Verify correct message from subscriber
+			Assert.IsTrue(firstSubscriberHit); // The first subscriber will be hit
+			Assert.IsFalse(thirdSubscriberHit); // Third subscriber will not be hit, missed due to thrown exception.
+		}
 
-        private void CustomTestEventMethodHandler(object sender, CustomTestEvent customTestEvent)
-        {
-            Assert.AreEqual("Custom Event", customTestEvent.Name);
-            Assert.AreEqual(1, customTestEvent.Identifier);
-            _methodHandlerHit = true;
-        }
-    }
+		private void CustomTestEventMethodHandler(object sender, CustomTestEvent customTestEvent)
+		{
+			Assert.AreEqual("Custom Event", customTestEvent.Name);
+			Assert.AreEqual(1, customTestEvent.Identifier);
+			_methodHandlerHit = true;
+		}
+	}
 
-    internal class CustomTestEvent : IEventBusEventArgs
-    {
-        public string Name { get; set; }
-        public int Identifier { get; set; }
-    }
+	internal class CustomTestEvent : IEventBusEventArgs
+	{
+		public string Name { get; set; }
+		public int Identifier { get; set; }
+	}
 }
