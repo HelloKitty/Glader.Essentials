f1808e57fc7377a8bfd4dc34896ef9d49e3a212e
HelloKitty <andrew.blakely@ymail.com>
2021-09-23 14:35:48 -0500

    Added IMainThreadDeterminable

---
+++ src/Glader.Essentials.Common/Utility/IMainThreadDeterminable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Strategy for determining if you're on the main thread of the application.
+	/// </summary>
+	public interface IMainThreadDeterminable
+	{
+		/// <summary>
+		/// Indicates if the current thread is the main thread.
+		/// </summary>
+		abstract bool IsMainThread { get; }
+	}
+}
