7366172cd7e4e80912a11441b739f7614f38e96f
HelloKitty <andrew.blakely@ymail.com>
2021-09-23 13:29:53 -0500

    Added EngineEventBusListener

---
+++ src/Glader.Essentials.GameFramework/Event/EventBus/EngineEventListener.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Engine/framework implementation of <see cref="EventBusListener{TEventArgsType}"/> that implements <see cref="IGameInitializable"/>
+	/// virtually. Allowing the event listeners to exist in the engine system.
+	/// </summary>
+	/// <typeparam name="TEventArgsType">The args type.</typeparam>
+	public abstract class EngineEventBusListener<TEventArgsType> : EventBusListener<TEventArgsType>, IGameInitializable
+		where TEventArgsType : IEventBusEventArgs
+	{
+		/// <summary>
+		/// Creates a new engine event bus listener that listens for events
+		/// on the specified bus.
+		/// </summary>
+		/// <param name="bus">The event bus to listen to.</param>
+		protected EngineEventBusListener(IEventBus bus)
+			: base(bus)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public virtual Task OnGameInitialized()
+		{
+			//Do nothing, this puts it into the Engine system.
+			return Task.CompletedTask;
+		}
+	}
+}
