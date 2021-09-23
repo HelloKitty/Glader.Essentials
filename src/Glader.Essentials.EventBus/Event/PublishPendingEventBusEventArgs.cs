9a15fd95c62c1b5c938f7c32788ec6131d7f44c1
HelloKitty <andrew.blakely@ymail.com>
2021-09-23 14:42:27 -0500

    Added PublishPendingEventBusEventArgs

---
+++ src/Glader.Essentials.EventBus/Event/PublishPendingEventBusEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event that can be sent to an <see cref="IEventBus"/> to force it to publish event args.
+	/// </summary>
+	public record PublishPendingEventBusEventArgs : IEventBusEventArgs;
+}
