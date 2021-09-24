0d37bc2cd9408999964ae436e526f22a3f16e759
HelloKitty <andrew.blakely@ymail.com>
2021-09-23 19:20:45 -0500

    Added non-subscription interface version of IEventPublisher

--- src/Glader.Essentials.Events/IEventPublisher.cs
+++ src/Glader.Essentials.Events/IEventPublisher.cs
@@ -4,13 +4,12 @@ using System.Text;
 
 namespace Glader.Essentials
 {
+	// Created this simplified version to support event interface-less event publishing.
 	/// <summary>
 	/// Contract for a type that publishes a particular event interface type.
 	/// </summary>
 	/// <typeparam name="TEventArgsType">The event args type name.</typeparam>
-	/// <typeparam name="TEventPublisherInterface">The even publisher interface.</typeparam>
-	public interface IEventPublisher<TEventPublisherInterface, in TEventArgsType> //One day I dream of a generic type arg that can be constrained to a type in the implementing types hierarchy.
-		where TEventArgsType : EventArgs
+	public interface IEventPublisher<in TEventArgsType>
 	{
 		/// <summary>
 		/// Publishes the event with the args <see cref="eventArgs"/>
@@ -19,5 +18,16 @@ namespace Glader.Essentials
 		/// <param name="sender">The sender of the event.</param>
 		/// <param name="eventArgs">The arguments for the event.</param>
 		void PublishEvent(object sender, TEventArgsType eventArgs);
+	}
+
+	/// <summary>
+	/// Contract for a type that publishes a particular event interface type.
+	/// </summary>
+	/// <typeparam name="TEventArgsType">The event args type name.</typeparam>
+	/// <typeparam name="TEventPublisherInterface">The even publisher interface.</typeparam>
+	public interface IEventPublisher<TEventPublisherInterface, in TEventArgsType> : IEventPublisher<TEventArgsType>
+		where TEventArgsType : EventArgs
+	{
+
 	}
 }
