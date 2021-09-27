ebfddbfa20b08874b089ba799b79bf5175000793
HelloKitty <andrew.blakely@ymail.com>
2021-09-27 14:08:06 -0500

    Added event phases to EventBus listener

--- src/Glader.Essentials.EventBus/Listeners/EventBusListener.cs
+++ src/Glader.Essentials.EventBus/Listeners/EventBusListener.cs
@@ -41,6 +41,80 @@ namespace Glader.Essentials
 		}
 
 		/// <summary>
+		/// Internally called event method raised by the event bus.
+		/// Handles before/after logic.
+		/// </summary>
+		/// <param name="sender">The sender of the event (may be null).</param>
+		/// <param name="args">The event args.</param>
+		private void InternalOnEventFired(object sender, TEventArgsType args)
+		{
+			// If OnBefore returns false then we don't continue handling the event.
+			if(!OnBeforeEventFired(sender, args))
+				return;
+
+			HandleEvent(sender, args);
+		}
+
+		/// <summary>
+		/// Called internally to service an event after <see cref="OnBeforeEventFired"/>.
+		/// </summary>
+		/// <param name="sender">The sender of the event (may be null).</param>
+		/// <param name="args">The event args.</param>
+		protected void HandleEvent(object sender, TEventArgsType args)
+		{
+			bool successful = true;
+			try
+			{
+				OnEventFired(sender, args);
+			}
+			catch (Exception e)
+			{
+				successful = false;
+				OnException(sender, args, e);
+			}
+			finally
+			{
+				OnAfterEventFired(sender, args, successful);
+			}
+		}
+
+		/// <summary>
+		/// Implementer can override this function to handle encountered exceptions.
+		/// </summary>
+		/// <param name="sender">The sender of the event (may be null).</param>
+		/// <param name="args">The event args.</param>
+		/// <param name="error"></param>
+		protected virtual void OnException(object sender, TEventArgsType args, Exception error)
+		{
+			return;
+		}
+
+		/// <summary>
+		/// Implementer can override this function to handle post-event handling logic.
+		/// Always called, regardless of if an error was encountered.
+		/// </summary>
+		/// <param name="sender">The sender of the event (may be null).</param>
+		/// <param name="args">The event args.</param>
+		/// <param name="successful">Indicates if the event was handled without exception.</param>
+		protected virtual void OnAfterEventFired(object sender, TEventArgsType args, bool successful)
+		{
+			return;
+		}
+
+		/// <summary>
+		/// Implementer can override this function to handle pre-event handling logic.
+		/// Return false to prevent continue of handling of the event.
+		/// Exceptions thrown here are uncaught by the handler.
+		/// </summary>
+		/// <param name="sender">The sender of the event (may be null).</param>
+		/// <param name="args">The event args.</param>
+		/// <returns>Indicates if the event should be handled.</returns>
+		protected virtual bool OnBeforeEventFired(object sender, TEventArgsType args)
+		{
+			return true;
+		}
+
+		/// <summary>
 		/// Called when the event bus fires an event.
 		/// </summary>
 		/// <param name="sender">The sender of the event (may be null).</param>
@@ -62,7 +136,7 @@ namespace Glader.Essentials
 					throw new InvalidOperationException($"Cannot {nameof(Subscribe)} multiple times in {GetType().Name}. Subscriptions should only occur once..");
 
 				IsSubscribed = true;
-				return bus.Subscribe<TEventArgsType>(OnEventFired);
+				return bus.Subscribe<TEventArgsType>(InternalOnEventFired);
 			}
 		}
 
