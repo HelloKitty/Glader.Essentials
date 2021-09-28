eb42376195f49f9479679a98fa0d69f038ad2147
HelloKitty <andrew.blakely@ymail.com>
2021-09-27 23:39:23 -0500

    Added simplified ElementClickedEventListener types

---
+++ src/Glader.Essentials.Unity/Event/EventBus/ElementClickedEventListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event listener that will subscribe to the provided element's click event.
+	/// Using <see cref="OnElementClickedEventArgs"/> with <see cref="IUIElement"/> as the source.
+	/// </summary>
+	public abstract class ElementClickedEventListener : EventBusListener<OnElementClickedEventArgs, IUIElement>
+	{
+		/// <summary>
+		/// Creates a new click listener on the provided element.
+		/// </summary>
+		/// <param name="element">Element to listen on.</param>
+		protected ElementClickedEventListener(IUIElement element) 
+			: base(element.Bus)
+		{
+
+		}
+	}
+
+	/// <summary>
+	/// Async Event listener that will subscribe to the provided element's click event.
+	/// Using <see cref="OnElementClickedEventArgs"/> with <see cref="IUIElement"/> as the source.
+	/// </summary>
+	public abstract class ElementClickedEventListenerAsync : UnityAsyncEventBusListener<OnElementClickedEventArgs, IUIElement>
+	{
+		/// <summary>
+		/// Creates a new click listener on the provided element.
+		/// </summary>
+		/// <param name="element">Element to listen on.</param>
+		protected ElementClickedEventListenerAsync(IUIElement element)
+			: base(element.Bus)
+		{
+
+		}
+	}
+}
