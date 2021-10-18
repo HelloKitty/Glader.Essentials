3a9d477621c7822abac1d5f2bd0ee3cf6fe83939
HelloKitty <andrew.blakely@ymail.com>
2021-10-17 22:08:36 -0500

    Added BaseRegisterableUnityUI

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Base/BaseRegisterableUnityUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public abstract class BaseRegisterableUnityUI<TRegisterType> : BaseUnityUI, IUIAdapterRegisterable
+	{
+		//Assigned by Unity3D.
+		/// <summary>
+		/// Internally Unity3D serialized key.
+		/// </summary>
+		[Tooltip("Used to determine wiring for UI dependencies.")] [SerializeField]
+#pragma warning disable 649
+		private string _RegistrationKey;
+#pragma warning restore 649
+
+		/// <inheritdoc />
+		public string RegistrationKey => _RegistrationKey;
+
+		/// <inheritdoc />
+		public Type UIServiceType => typeof(TRegisterType);
+
+		/// <inheritdoc />
+		public virtual bool AsKeyed { get; } = true;
+	}
+}
