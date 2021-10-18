fa880c43751d54802ac3ea201230743866ad88c7
HelloKitty <andrew.blakely@ymail.com>
2021-10-17 22:05:47 -0500

    Improved the IUIInput implementation

---
+++ src/Glader.Essentials.UI/Interface/Elements/IUIInputField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for UI elements that implement Input Field functionality.
+	/// </summary>
+	public interface IUIInputField : IUIText
+	{
+
+	}
+}
--- src/Glader.Essentials.Unity/UI/Abstraction/Adapters/UnityInputFieldUITextAdapter.cs
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/UnityInputFieldUITextAdapter.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Glader.Essentials
 {
 	/// <summary>
-	/// Adapter for <see cref="InputField"/> to <see cref="IUIText"/>
+	/// Base adapter for Input Field <see cref="IUIInputField"/> types.
 	/// </summary>
-	public sealed class UnityInputFieldUITextAdapter : BaseUnityUIAdapter<InputField, IUIText>, IUIText
+	/// <typeparam name="TComponentType">The backing component type.</typeparam>
+	public abstract class BaseUnityInputFieldAdapter<TComponentType> : BaseUnityUIAdapter<TComponentType, IUIInputField>, IUIInputField
+		where TComponentType : UnityEngine.EventSystems.UIBehaviour
 	{
 		/// <inheritdoc />
-		public string Text
+		public abstract string Text { get; set; }
+	}
+
+	/// <summary>
+	/// Adapter for <see cref="InputField"/> to <see cref="IUIInputField"/>
+	/// </summary>
+	public class UnityInputFieldUITextAdapter : BaseUnityInputFieldAdapter<InputField>, IUIInputField
+	{
+		/// <inheritdoc />
+		public override string Text
 		{
 			get => UnityUIObject.text;
 			set => UnityUIObject.text = value;
