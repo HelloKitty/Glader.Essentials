d75bb1e7d45327df6bd5072032d6ebbdb8f48f01
HelloKitty <andrew.blakely@ymail.com>
2021-10-17 22:08:51 -0500

    Added UI Window system

---
+++ src/Glader.Essentials.UI/Interface/Containers/IUIWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// The container for all frames.
+	/// </summary>
+	public interface IUIWindow
+	{
+
+	}
+
+	/// <summary>
+	/// The container for all frames.
+	/// </summary>
+	public interface IUIWindow<out TRootFrameType> : IUIWindow
+		where TRootFrameType : IUIFrame
+	{
+		/// <summary>
+		/// The root/default frame that contains all free-floating elements.
+		/// </summary>
+		TRootFrameType Root { get; }
+	}
+}
---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/Base/BaseUIWindowAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Base-type for <see cref="IUIWindow"/> adapters.
+	/// </summary>
+	/// <typeparam name="TWindowType">The window type.</typeparam>
+	public abstract class BaseUIWindowAdapter<TWindowType> : BaseRegisterableUnityUI<TWindowType>, IUIWindow
+		where TWindowType : IUIWindow
+	{
+		/// <inheritdoc />
+		public override bool AsKeyed { get; } = false;
+
+		[Button]
+		public void TryLocateFrames()
+		{
+			foreach (FieldInfo fi in GetType()
+				.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+			{
+				if (fi.GetCustomAttribute<SerializeField>() == null)
+					continue;
+
+				if (!typeof(IUIFrame).IsAssignableFrom(fi.FieldType))
+					continue;
+
+				//Because of active load scene, we have to iterate each scene
+				foreach(var go in SceneManager.GetSceneByBuildIndex(gameObject.scene.buildIndex).GetRootGameObjects())
+				{
+					foreach(var registerable in go.GetComponentsInChildren<GladerBehaviour>(true)
+						.Where(m => fi.FieldType.IsAssignableFrom(m.GetType())))
+					{
+						fi.SetValue(this, registerable);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
