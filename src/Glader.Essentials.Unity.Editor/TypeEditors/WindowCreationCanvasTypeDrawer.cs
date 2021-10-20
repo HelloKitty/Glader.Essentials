7475ce31a4738b76a3ac7d461d70c36c2c6c5dc8
HelloKitty <andrew.blakely@ymail.com>
2021-10-20 11:09:43 -0500

    Added indevelopment WindowCreationCanvasTypeDrawer

---
+++ src/Glader.Essentials.Unity.Editor/TypeEditors/WindowCreationCanvasTypeDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Glader.Essentials
+{
+	[CustomEditor(typeof(Canvas), true)]
+	public class WindowCreationCanvasTypeDrawer : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			if(GUILayout.Button("Create UIWindow"))
+			{
+				CreateWindowSource();
+			}
+		}
+
+		private void CreateWindowSource()
+		{
+			throw new NotImplementedException();
+			Canvas canvas = (Canvas) target;
+			var scene = SceneManager.GetSceneByBuildIndex(canvas.gameObject.scene.buildIndex);
+
+			foreach(var go in scene.GetRootGameObjects())
+			{
+
+			}
+		}
+	}
+}
