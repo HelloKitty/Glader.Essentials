b44ceb475f2dbf7bec450cfa3fbbe59bf6f8b1f6
HelloKitty <andrew.blakely@ymail.com>
2021-10-26 18:34:13 -0500

    Created IEventBusPublishable and moved EventBus publishing there

---
+++ src/Glader.Essentials.EventBus/EventBus/IEventBusPublishable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for EventBus publishing functionality.
+	/// </summary>
+	public interface IEventBusPublishable
+	{
+		/// <summary>
+		/// Publishes the specified event to any subscribers for the event type.
+		/// </summary>
+		/// <typeparam name="TEventType">The type of event</typeparam>
+		/// <param name="sender">The sender of the event.</param>
+		/// <param name="eventData">Event to publish</param>
+		void Publish<TEventType>(object sender, TEventType eventData)
+			where TEventType : IEventBusEventArgs;
+	}
+}
