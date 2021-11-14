431b592a5d1122ac349f1ccc8efd11af7394809e
HelloKitty <andrew.blakely@ymail.com>
2021-11-14 14:22:05 -0600

    Added INetworkTimeService

---
+++ src/Glader.Essentials.Common/Time/INetworkTimeService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public interface INetworkTimeService : IReadonlyNetworkTimeService
+	{
+		/// <summary>
+		/// Sets the time synchronization for the network time service.
+		/// This will initialize the properties exposed for time usage.
+		/// </summary>
+		/// <param name="originalLocalTime">The original local time sent in the synchronization packet.</param>
+		/// <param name="remoteTime">The remote time sent back.</param>
+		void SetTimeSynchronization(long originalLocalTime, long remoteTime);
+
+		/// <summary>
+		/// Recalculates the time stamp for when a time query was sent.
+		/// (current local time.
+		/// </summary>
+		void RecalculateQueryTime();
+
+		/// <summary>
+		/// Represents the timestamp from the last time query.
+		/// </summary>
+		long LastQueryTime { get; }
+	}
+
+	public interface IReadonlyNetworkTimeService
+	{
+		/// <summary>
+		/// Calculates the RTT (round-trip-time) from a provided initial <see cref="originalLocalTime"/>.
+		/// </summary>
+		/// <param name="originalLocalTime">The original local time.</param>
+		/// <returns>The RTT (2 * latency).</returns>
+		long CalculateRoundTripTime(long originalLocalTime);
+
+		/// <summary>
+		/// Calculates the offset between the remote and client clocks.
+		/// This offset can be used to convert remote times to
+		/// client times.
+		/// </summary>
+		/// <param name="originalLocalTime"></param>
+		/// <param name="remoteTime"></param>
+		/// <returns></returns>
+		long CalculateTimeOffset(long originalLocalTime, long remoteTime);
+
+		/// <summary>
+		/// The current computed time offset.
+		/// (Does no computation).
+		/// Includes Latency.
+		/// </summary>
+		long CurrentTimeOffset { get; }
+
+		/// <summary>
+		/// The current local time.
+		/// (Depending on implementation this could be expensive to call).
+		/// </summary>
+		long CurrentLocalTime { get; }
+
+		/// <summary>
+		/// The current computed latency.
+		/// (Does no computation).
+		/// </summary>
+		long CurrentLatency { get; }
+
+		/// <summary>
+		/// The approximated current remote time.
+		/// Likely based on the <see cref="CurrentLocalTime"/> and the <see cref="CurrentTimeOffset"/>.
+		/// </summary>
+		long CurrentRemoteTime { get; }
+
+		/// <summary>
+		/// Represents the milliseconds since application startup.
+		/// </summary>
+		int MillisecondsSinceStartup { get; }
+	}
+}
