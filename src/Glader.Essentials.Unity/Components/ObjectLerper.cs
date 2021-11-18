c447f9de89d85444b5e9d164fae8637e025bdc2d
HelloKitty <andrew.blakely@ymail.com>
2021-11-17 22:32:16 -0600

    Added ObjectLerper

---
+++ src/Glader.Essentials.Unity/Components/ObjectLerper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	//From GladMMO: Based on AvatarLerper
+	public sealed class ObjectLerper : MonoBehaviour
+	{
+		private Vector3 lastPosition;
+
+		private Quaternion lastRotation;
+
+		[SerializeField] 
+		private Transform TransformToFollow;
+
+		[SerializeField] 
+		private float LerpPower = 0.7f;
+
+		[SerializeField] 
+		private bool UseLateUpdate = true;
+
+		void Start()
+		{
+			lastPosition = transform.position;
+			lastRotation = transform.rotation;
+		}
+
+		void Update()
+		{
+			if (!UseLateUpdate)
+				Lerp();
+		}
+
+		void LateUpdate()
+		{
+			if (UseLateUpdate)
+				Lerp();
+		}
+
+		private void Lerp()
+		{
+			lastPosition = transform.position = Vector3.Slerp(lastPosition, TransformToFollow.position, LerpPower * Time.deltaTime);
+			lastRotation = transform.rotation = Quaternion.Slerp(lastRotation, TransformToFollow.rotation, LerpPower * Time.deltaTime);
+		}
+	}
+}
