127c082c7e8effdc71d4a7479a1359ba0bb8d7da
HelloKitty <andrew.blakely@ymail.com>
2021-11-17 22:20:47 -0600

    Added TransformReplicateComponent

---
+++ src/Glader.Essentials.Unity/Components/TransformReplicateComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public sealed class TransformReplicateComponent : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform TransformToReplicate;
+
+		private void Update()
+		{
+			//We just check the rotation of the transform and match it.
+			//TODO: Handle position for "leaning forward" in VR, if that's even a thing
+			transform.rotation = TransformToReplicate.rotation;
+		}
+	}
+}
