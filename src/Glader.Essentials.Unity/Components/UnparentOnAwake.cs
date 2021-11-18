6235f83bbbec27bd65bfb979d8bd5562859cfc80
HelloKitty <andrew.blakely@ymail.com>
2021-11-17 22:21:22 -0600

    Added UnparentOnAwake

---
+++ src/Glader.Essentials.Unity/Components/UnparentOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public sealed class UnparentOnAwake : MonoBehaviour
+	{
+		private void Awake()
+		{
+			gameObject.transform.parent = null;
+		}
+	}
+}
