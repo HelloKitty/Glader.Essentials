7ba120095340581561adfe7d5370dfbbd26cbe2b
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 00:57:18 -0600

    Added DefaultEventSubscriptionIterationStrategy

---
+++ src/Glader.Essentials.EventBus/EventBus/Publishing/DefaultEventSubscriptionIterationStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Default implementation of <see cref="IEventSubscriptionIterationStrategy"/>.
+	/// </summary>
+	public sealed class DefaultEventSubscriptionIterationStrategy : IEventSubscriptionIterationStrategy
+	{
+		/// <summary>
+		/// Shared instance of <see cref="DefaultEventSubscriptionIterationStrategy"/>
+		/// </summary>
+		public static DefaultEventSubscriptionIterationStrategy Instance { get; } = new();
+
+		/// <inheritdoc />
+		public IEnumerable<IEventBusSubscription> Enumerate(IEventBusSubscription[] subscriptions)
+		{
+			foreach (var entry in subscriptions)
+				yield return entry;
+		}
+	}
+}
