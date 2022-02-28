489ab583796a7da5df79e29139f0d92a5c9e599f
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 02:54:09 -0600

    Added IUIMouseOverExitEventListener

---
+++ src/Glader.Essentials.Unity/UI/Event/IUIMouseOverExitEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for UI that can receive events.
+	/// </summary>
+	public interface IUIMouseOverExitEventListener
+	{
+		/// <summary>
+		/// See: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnMouseExit.html
+		/// </summary>
+		void OnMouseExit();
+	}
+}
