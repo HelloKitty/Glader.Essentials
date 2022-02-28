13c28e4c7ac1c1f791463efe2e2a2b1a7bd9720d
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 02:55:09 -0600

    Added IUIMouseBoundsEventListener

---
+++ src/Glader.Essentials.Unity/UI/Event/IUIMouseBoundsEventListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for UI that can receive events.
+	/// </summary>
+	public interface IUIMouseBoundsEventListener 
+		: IUIMouseOverExitEventListener, IUIMouseOverEnterEventListener
+	{
+
+	}
+}
