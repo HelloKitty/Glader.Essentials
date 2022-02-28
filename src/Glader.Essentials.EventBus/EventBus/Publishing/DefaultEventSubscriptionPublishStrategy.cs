68452f02f102023360f12d7062c92756ceca9870
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 00:57:12 -0600

    Added DefaultEventSubscriptionPublishStrategy

---
+++ src/Glader.Essentials.EventBus/EventBus/Publishing/DefaultEventSubscriptionPublishStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Default implementation of <see cref="IEventSubscriptionPublishStrategy"/>.
+	/// </summary>
+	public sealed class DefaultEventSubscriptionPublishStrategy : IEventSubscriptionPublishStrategy
+	{
+		/// <summary>
+		/// Shared instance of <see cref="DefaultEventSubscriptionPublishStrategy"/>
+		/// </summary>
+		public static DefaultEventSubscriptionPublishStrategy Instance { get; } = new();
+
+		/// <inheritdoc />
+		public bool Publish<TEventType>(IEventBusSubscription subscription, object sender, TEventType eventData) where TEventType : IEventBusEventArgs
+		{
+			//The subscription could be null because we REUSE indexes and set them as null
+			//to avoid allocations if possible on unsubscription or subscriptions
+			subscription?.Publish(sender, eventData);
+			return true;
+		}
+	}
+}
