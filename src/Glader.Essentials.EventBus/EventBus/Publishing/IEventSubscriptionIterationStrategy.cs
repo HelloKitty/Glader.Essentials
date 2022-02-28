8f6dbcc2fd3cbb7d3a48b70e5d76a3991440b171
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 00:56:40 -0600

    Added IEventSubscriptionIterationStrategy

---
+++ src/Glader.Essentials.EventBus/EventBus/Publishing/IEventSubscriptionIterationStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Strategy for iteration of the event subscription.
+	/// </summary>
+	public interface IEventSubscriptionIterationStrategy
+	{
+		/// <summary>
+		/// Enumerates the provided subscriptions.
+		/// </summary>
+		/// <param name="subscriptions">The subscriptions to iterate.</param>
+		/// <returns>Enumerate subscription.</returns>
+		IEnumerable<IEventBusSubscription> Enumerate(IEventBusSubscription[] subscriptions);
+	}
+}
