29eb61f2fee5421f7e16c3b293a1caa89f922262
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 00:56:33 -0600

    Added IEventSubscriptionPublishStrategy

---
+++ src/Glader.Essentials.EventBus/EventBus/Publishing/IEventSubscriptionPublishStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for event the subscription publishing strategy.
+	/// </summary>
+	public interface IEventSubscriptionPublishStrategy
+	{
+		/// <summary>
+		/// Publishes the event data through the provided subscription.
+		/// </summary>
+		/// <typeparam name="TEventType">The event args.</typeparam>
+		/// <param name="subscription">The event subscription.</param>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="eventData">The event data.</param>
+		/// <returns>True if publishing should continue.</returns>
+		bool Publish<TEventType>(IEventBusSubscription subscription, object sender, TEventType eventData)
+			where TEventType : IEventBusEventArgs;
+	}
+}
