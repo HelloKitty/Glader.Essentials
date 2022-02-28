c49ef06dd87651e60c0aa1d7e4ed0a0efdb5c33c
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 02:56:06 -0600

    Added more doc to UIMouseOver event interfaces

--- src/Glader.Essentials.Unity/UI/Event/IUIMouseOverExitEventListener.cs
+++ src/Glader.Essentials.Unity/UI/Event/IUIMouseOverExitEventListener.cs
@@ -6,6 +6,7 @@ namespace Glader.Essentials
 {
 	/// <summary>
 	/// Contract for UI that can receive events.
+	/// Implementing this interface allows for the Unity3D engine to callback.
 	/// </summary>
 	public interface IUIMouseOverExitEventListener
 	{
