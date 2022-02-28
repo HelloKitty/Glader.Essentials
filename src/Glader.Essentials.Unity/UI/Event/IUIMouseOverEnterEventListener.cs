4b474f5b0bb0780d9d1408d63cb31bfe831fe7f3
HelloKitty <andrew.blakely@ymail.com>
2022-02-28 02:54:15 -0600

    Added IUIMouseOverEnterEventListener

---
+++ src/Glader.Essentials.Unity/UI/Event/IUIMouseOverEnterEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for UI that can receive events.
+	/// </summary>
+	public interface IUIMouseOverEnterEventListener
+	{
+		/// <summary>
+		/// See: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnMouseOver.html
+		/// </summary>
+		void OnMouseOver();
+	}
+}
