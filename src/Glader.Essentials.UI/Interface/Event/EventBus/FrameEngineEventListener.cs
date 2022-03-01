da951215a27589013b35fed4456b5f4b4b62281a
HelloKitty <andrew.blakely@ymail.com>
2022-03-01 00:37:40 -0600

    Added FrameEngineEventListener

---
+++ src/Glader.Essentials.UI/Interface/Event/EventBus/FrameEngineEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Implementation of <see cref="EngineEventBusListener{TEventArgsType, TSourceType}"/> that uses
+	/// the provided <see cref="IUIFrame"/>'s <see cref="IEventBus"/> to listen to the events.
+	/// </summary>
+	/// <typeparam name="TEventArgsType"></typeparam>
+	/// <typeparam name="TSourceType"></typeparam>
+	public abstract class FrameEngineEventListener<TEventArgsType, TSourceType> : EngineEventBusListener<TEventArgsType, TSourceType>
+		where TEventArgsType : IEventBusEventArgs
+		where TSourceType : IUIFrame
+	{
+		/// <summary>
+		/// The frame the <see cref="EngineEventListener{TSubscribableType}"/> is attached to.
+		/// </summary>
+		protected TSourceType Frame { get; }
+
+		/// <inheritdoc />
+		protected FrameEngineEventListener(TSourceType frame) 
+			: base(frame.Bus)
+		{
+			Frame = frame ?? throw new ArgumentNullException(nameof(frame));
+		}
+	}
+}
