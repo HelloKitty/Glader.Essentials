8fee319668ac923f7133f5391f9ca9f64c533cf2
HelloKitty <andrew.blakely@ymail.com>
2022-09-17 11:44:52 -0500

    Added SceneDisposableAttribute

---
+++ src/Glader.Essentials.GameFramework/Attributes/SceneDisposableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Indicates a <see cref="SceneTypeCreateAttribute"/> marked with <see cref="IDisposable"/>
+	/// should have it registered as Disposable.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public sealed class SceneDisposableAttribute : Attribute
+	{
+
+	}
+}
