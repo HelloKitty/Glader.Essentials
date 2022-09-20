1d97ba5186b9c09311d1306e815a8634aa0485b1
HelloKitty <andrew.blakely@ymail.com>
2022-09-20 09:19:06 -0500

    Added ToArrayTryAvoidCopy

--- src/Glader.Essentials.Common/LINQ/IEnumerableExtensions.cs
+++ src/Glader.Essentials.Common/LINQ/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Glader.Essentials
@@ -25,6 +26,23 @@ namespace Glader.Essentials
 				action(t);
 
 			return enumerable;
+		}
+
+		/// <summary>
+		/// Extension that attempts to downcast the provided <see cref="collection"/>
+		/// to an array if it is an array. Otherwise it will call <see cref="Enumerable"/>.ToArray().
+		/// </summary>
+		/// <typeparam name="T">The element type of the collection.</typeparam>
+		/// <param name="collection">The collection to cast.</param>
+		/// <returns></returns>
+		public static T[] ToArrayTryAvoidCopy<T>(this IEnumerable<T> collection)
+		{
+			if(collection == null) throw new ArgumentNullException(nameof(collection));
+
+			if(collection is T[] downcastedArray)
+				return downcastedArray;
+
+			return collection.ToArray();
 		}
 	}
 }
