22d8ef21f8db6a7227876463ca5338cd60021fce
HelloKitty <andrew.blakely@ymail.com>
2023-03-26 00:01:40 -0500

    Moved Image Sprite setting into seperate interface

--- src/Glader.Essentials.Unity/UI/Interface/IUIImage.cs
+++ src/Glader.Essentials.Unity/UI/Interface/IUIImage.cs
@@ -9,18 +9,8 @@ namespace Glader.Essentials
 	/// <summary>
 	/// Contract for types that implement UI image.
 	/// </summary>
-	public interface IUIImage : IUIElement, IUIUnityColorable
+	public interface IUIImage : IUIElement, IUIUnityColorable, IUISpriteSettable
 	{
-		/// <summary>
-		/// Sets the texture for the current sprite.
-		/// </summary>
-		/// <param name="texture"></param>
-		void SetSpriteTexture(Texture2D texture);
-
-		/// <summary>
-		/// Sets the texture for the current sprite async.
-		/// </summary>
-		/// <param name="textureAwaitable">Awaitable that can produce a <see cref="Texture2D"/>.</param>
-		Task SetSpriteTextureAsync(Task<Texture2D> textureAwaitable);
+		
 	}
 }
---
+++ src/Glader.Essentials.Unity/UI/Interface/IUISpriteSettable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public interface IUISpriteSettable
+	{
+		/// <summary>
+		/// Sets the texture for the current sprite.
+		/// </summary>
+		/// <param name="texture"></param>
+		void SetSpriteTexture(Texture2D texture);
+
+		/// <summary>
+		/// Sets the texture for the current sprite async.
+		/// </summary>
+		/// <param name="textureAwaitable">Awaitable that can produce a <see cref="Texture2D"/>.</param>
+		Task SetSpriteTextureAsync(Task<Texture2D> textureAwaitable);
+	}
+}
