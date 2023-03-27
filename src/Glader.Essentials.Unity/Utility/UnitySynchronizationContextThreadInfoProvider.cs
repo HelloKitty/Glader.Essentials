d8c9c000140f39dae1385801f41ac0e7281d0557
HelloKitty <andrew.blakely@ymail.com>
2023-03-27 08:21:31 -0500

    Move SyncContext implementation of IMainThreadDeterminable to UnitySynchronizationContextThreadInfoProvider

---
+++ src/Glader.Essentials.Unity/Utility/UnitySynchronizationContextThreadInfoProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Info providing object that implements <see cref="IMainThreadDeterminable"/>.
+	/// </summary>
+	public sealed class UnitySynchronizationContextThreadInfoProvider : IMainThreadDeterminable
+	{
+		/// <inheritdoc />
+		public bool IsMainThread => UnityAsyncHelper.UnityMainThreadContext == SynchronizationContext.Current;
+	}
+}
