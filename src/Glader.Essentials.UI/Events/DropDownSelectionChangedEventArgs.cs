16893bd840d4578d3225c2dc61be0614a34b059c
HelloKitty <andrew.blakely@ymail.com>
2023-06-08 02:44:01 -0500

    Added DropDownSelectionChangedEventArgs

---
+++ src/Glader.Essentials.UI/Events/DropDownSelectionChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event fired when a <see cref="IUIDropDown"/> selected changes.
+	/// </summary>
+	public sealed record DropDownSelectionChangedEventArgs(int Index, string Value)
+		: IEventBusEventArgs;
+
+}
