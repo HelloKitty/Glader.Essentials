d11c2a51b50ea9cc9859e4bbf24f5bc191fcdae9
HelloKitty <andrew.blakely@ymail.com>
2023-06-16 05:32:02 -0500

    Removed old deprecated castable listener extensions

--- src/Glader.Essentials.UI/Interface/Extensions/IUIElementExtensions.cs
+++ src/Glader.Essentials.UI/Interface/Extensions/IUIElementExtensions.cs
@@ -8,30 +8,11 @@ namespace Glader.Essentials
 	public static class IUIElementExtensions
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private static EventBusSubscriptionToken CreateSubscription<TElementType, TEventType>(IUIEventListenable listenable, Action<TElementType, TEventType> callback) 
-			where TEventType : IEventBusEventArgs
-		{
-			return listenable.Bus
-				.Subscribe<TEventType>((sender, args) => callback?.Invoke((TElementType)sender, args));
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private static EventBusSubscriptionToken CreateSubscription<TEventType>(IUIEventListenable listenable, Action<object, TEventType> callback)
 			where TEventType : IEventBusEventArgs
 		{
 			return listenable.Bus
 				.Subscribe<TEventType>((sender, args) => callback?.Invoke(sender, args));
-		}
-
-		/// <summary>
-		/// Subscribes a callback for the button's click.
-		/// </summary>
-		/// <param name="element">The button to subscribe to.</param>
-		/// <param name="callback">The callback to register.</param>
-		public static EventBusSubscriptionToken OnClick<TClickableType>(this TClickableType element, Action<TClickableType, OnElementClickedEventArgs> callback)
-			where TClickableType : IUIClickable
-		{
-			return CreateSubscription(element, callback);
 		}
 
 		/// <summary>
