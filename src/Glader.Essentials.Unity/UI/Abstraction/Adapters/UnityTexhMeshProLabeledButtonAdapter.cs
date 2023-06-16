08a4ebeb274aea4ba5dafb85f7b972150739a8cc
HelloKitty <andrew.blakely@ymail.com>
2023-06-15 22:02:15 -0500

    Added UnityTexhMeshProLabeledButtonAdapter

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/UnityTexhMeshProLabeledButtonAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// See <see cref="IUILabeledButton"/>.
+	/// </summary>
+	public sealed class UnityTexhMeshProLabeledButtonAdapter : UnityButtonUIButtonAdapter, IUILabeledButton
+	{
+		[SerializeField]
+		private UnityTextMeshProUGUIUITextAdapter TextUIObject;
+
+		/// <inheritdoc />
+		public string Text
+		{
+			get => TextUIObject.Text;
+			set => TextUIObject.Text = value;
+		}
+
+		/// <inheritdoc />
+		public void SetColor(byte r, byte g, byte b, byte a)
+		{
+			TextUIObject.SetColor(r, g, b, a);
+		}
+	}
+}
