c93914402b330a2cd262cd041c5598437df9d480
HelloKitty <andrew.blakely@ymail.com>
2023-12-13 21:37:39 -0600

    Added SetBool extensions for Material properties

--- src/Glader.Essentials.Unity/Extensions/MaterialExtensions.cs
+++ src/Glader.Essentials.Unity/Extensions/MaterialExtensions.cs
@@ -7,5 +7,28 @@ namespace Glader.Essentials
 {
 	public static class MaterialExtensions
 	{
+		/// <summary>
+		/// Sets the bool value on the provided <see cref="Material"/>'s shader.
+		/// Using internally SetInt.
+		/// </summary>
+		/// <param name="mat">Material.</param>
+		/// <param name="id">The property id.</param>
+		/// <param name="value">The value.</param>
+		public static void SetBool(this Material mat, int id, bool value)
+		{
+			mat.SetInt(id, value ? 1 : 0);
+		}
+
+		/// <summary>
+		/// Sets the bool value on the provided <see cref="Material"/>'s shader.
+		/// Using internally SetInt.
+		/// </summary>
+		/// <param name="mat">Material.</param>
+		/// <param name="id">The property id.</param>
+		/// <param name="value">The value.</param>
+		public static void SetBool(this Material mat, string id, bool value)
+		{
+			mat.SetInt(id, value ? 1 : 0);
+		}
 	}
 }
