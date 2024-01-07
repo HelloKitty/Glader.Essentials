f5811f4276d6947a45c4f6d216b99268decbf650
HelloKitty <andrew.blakely@ymail.com>
2024-01-07 15:09:47 -0600

    Create IGameVariableRateTickable.cs

---
+++ src/Glader.Essentials.GameFramework/Engine/IGameVariableRateTickable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for types that should be ticked like <see cref="IGameTickable"/> but with only a specified <see cref="TickFrequency"/>.
+	/// </summary>
+	public interface IGameVariableRateTickable
+	{
+		/// <summary>
+		/// The duration of the wait/frequency of the tick.
+		/// </summary>
+		TimeSpan TickFrequency { get; }
+	}
+}
