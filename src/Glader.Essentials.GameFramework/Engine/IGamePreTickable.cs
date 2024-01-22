24bc995adf4a03b849d6ec5321bab65c7c3f8295
HelloKitty <andrew.blakely@ymail.com>
2024-01-22 02:59:53 -0600

    Create IGamePreTickable.cs

---
+++ src/Glader.Essentials.GameFramework/Engine/IGamePreTickable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Similar to <see cref="IGameTickable"/> but instead runs every tick before the tickables.
+	/// This is best used for initialization required for Update/Ticks.
+	/// </summary>
+	public interface IGamePreTickable
+	{
+		/// <summary>
+		/// Called right before the game ticks forward.
+		/// </summary>
+		void PreTick();
+	}
+}
