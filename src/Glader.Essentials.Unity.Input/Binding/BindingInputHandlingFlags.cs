088c59e63e60ada7584250ba9a3353a151bb8b30
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 09:04:36 -0600

    Create BindingInputHandlingFlags.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/BindingInputHandlingFlags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	[Flags]
+	public enum BindingInputHandlingFlags
+	{
+		None = 0,
+		OnPressed = 1 << 0,
+		OnReleased = 1 << 1
+	}
+}
