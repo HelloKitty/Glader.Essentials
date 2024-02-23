dc20b9aca07ebebe00f17b20874079496aba4503
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 13:17:35 -0600

    Create AlwaysFaceCameraComponent.cs

---
+++ src/Glader.Essentials.Unity/Components/AlwaysFaceCameraComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// From SwanSong.
+	/// Component will make the transform always face the camera.
+	/// </summary>
+	[RequireComponent(typeof(Transform))]
+	public sealed class AlwaysFaceCameraComponent : MonoBehaviour
+	{
+		[field: SerializeField]
+		public bool YAxisOnly { get; set; } = false;
+
+		private void LateUpdate()
+		{
+			if (Camera.main == null)
+				return;
+
+			if(YAxisOnly)
+			{
+				var currentRotation = transform.rotation.eulerAngles;
+				transform.rotation = Quaternion.Euler(currentRotation.x, Camera.main.transform.eulerAngles.y, currentRotation.z);
+			}
+			else
+			{
+				// Based on: https://forum.unity.com/threads/make-textmesh-face-camera.251840/
+				transform.rotation = Camera.main.transform.rotation;
+			}
+		}
+	}
+}
