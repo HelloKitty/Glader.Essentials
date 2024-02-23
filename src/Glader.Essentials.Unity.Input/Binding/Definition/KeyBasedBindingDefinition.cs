f932c12d4697ed70dccdc1ce1586c36ad049ffee
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 12:04:56 -0600

    Create KeyBasedBindingDefinition.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/Definition/KeyBasedBindingDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <inheritdoc />
+	public sealed record KeyBasedBindingDefinition(KeyCode Code, params KeyCode[] Modifiers) 
+		: BindingDefinition(Modifiers)
+	{
+		/// <inheritdoc />
+		public override bool IsPressed()
+		{
+			return Input.GetKeyDown(Code);
+		}
+
+		/// <inheritdoc />
+		public override bool IsReleased()
+		{
+			return Input.GetKeyUp(Code);
+		}
+
+		/// <inheritdoc />
+		public override bool IsHeld()
+		{
+			return Input.GetKey(Code);
+		}
+	}
+}
