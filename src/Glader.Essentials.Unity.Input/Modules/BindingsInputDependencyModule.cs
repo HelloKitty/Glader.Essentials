146dd43ad828356f6902d7de99f60df35375b1bc
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:56:23 -0600

    Create BindingsInputDependencyModule.cs

---
+++ src/Glader.Essentials.Unity.Input/Modules/BindingsInputDependencyModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Glader.Essentials
+{
+	/// <inheritdoc />
+	public sealed class BindingsInputDependencyModule<TBindingEnumType> : Module 
+		where TBindingEnumType : Enum
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			builder.RegisterModule<BindingInputControlsDependencyModule>()
+				.RegisterModule<BindingInputHandlerDependencyModule<TBindingEnumType>>();
+		}
+	}
+}
