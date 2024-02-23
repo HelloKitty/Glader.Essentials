1860f236450088cc832d8b0e81d08b2bb28e7774
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 12:07:48 -0600

    Create MouseBasedBindingDefinition.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/Definition/MouseBasedBindingDefinition.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Mouse-based implementation of <see cref="BindingDefinition"/>.
+	/// </summary>
+	public sealed record MouseBasedBindingDefinition(MouseButton Button, params KeyCode[] Modifiers) 
+		: BindingDefinition(Modifiers)
+	{
+		/*if (Input.GetMouseButton(0))
+		    Debug.Log("The left mouse button is being held down.");
+		if (Input.GetMouseButton(1))
+		    Debug.Log("The right mouse button is being held down.");
+		if (Input.GetMouseButton(2))
+		    Debug.Log("The middle mouse button is being held down.");
+		*/
+
+		/// <inheritdoc />
+		public override bool IsPressed()
+		{
+			switch(Button)
+			{
+				case MouseButton.LeftMouse:
+					return Input.GetMouseButtonDown(0); // left
+				case MouseButton.RightMouse:
+					return Input.GetMouseButtonDown(1); // right
+				case MouseButton.MiddleMouse:
+					return Input.GetMouseButtonDown(2); // middle
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool IsReleased()
+		{
+			switch(Button)
+			{
+				case MouseButton.LeftMouse:
+					return Input.GetMouseButtonUp(0); // left
+				case MouseButton.RightMouse:
+					return Input.GetMouseButtonUp(1); // right
+				case MouseButton.MiddleMouse:
+					return Input.GetMouseButtonUp(2); // middle
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		/// <inheritdoc />
+		public override bool IsHeld()
+		{
+			switch(Button)
+			{
+				case MouseButton.LeftMouse:
+					return Input.GetMouseButton(0); // left
+				case MouseButton.RightMouse:
+					return Input.GetMouseButton(1); // right
+				case MouseButton.MiddleMouse:
+					return Input.GetMouseButton(2); // middle
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+}
