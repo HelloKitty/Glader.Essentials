8c29449ecde4dc6dbe63f0ba5114934815cf666c
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 08:55:49 -0600

    Create KeyBindingInputChangedEventArgs.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/KeyBindingInputChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event fired when a keybinding is pressed.
+	/// </summary>
+	public sealed record KeyBindingInputChangedEventArgs<TBindingEnumType>(TBindingEnumType Type, bool Down) : IEventBusEventArgs
+		where TBindingEnumType : Enum;
+}
