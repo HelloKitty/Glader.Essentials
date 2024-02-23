383675d6c3564bb75d43ea13f9e1382930d9b065
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:34:18 -0600

    Create IMovementInputController.cs

---
+++ src/Glader.Essentials.Unity.Input/Input/Movement/IMovementInputController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	// From SwanSong
+	/// <summary>
+	/// Movement input version of <see cref="IAxisInputController"/>.
+	/// </summary>
+	public interface IMovementInputController : IAxisInputController
+	{
+
+	}
+}
