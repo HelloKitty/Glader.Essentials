22cabb2f0e0f27aa12616c2fde25bfa9f6bc5681
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 09:26:54 -0600

    Create IBindingInputMessageHandlingService.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/IBindingInputMessageHandlingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for a message handling service that can handle <see cref="IBindingInputMessageHandler{TBindingEnumType}"/>
+	/// </summary>
+	public interface IBindingInputMessageHandlingService<TBindingEnumType> 
+		where TBindingEnumType : Enum
+	{
+		/// <summary>
+		/// Processes the provided binding input data <see cref="message"/>.
+		/// </summary>
+		/// <param name="message"></param>
+		void Process(KeyBindingInputChangedEventArgs<TBindingEnumType> message);
+	}
+}
