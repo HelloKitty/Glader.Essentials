cc71396f09382a74c06da69d4002a4008f09d709
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:48:42 -0600

    Create IBindingSystemCameraInputController.cs

---
+++ src/Glader.Essentials.Unity.Input/Input/Camera/IBindingSystemCameraInputController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <inheritdoc />
+	public interface IBindingSystemCameraInputController : ICameraInputController
+	{
+		/// <summary>
+		/// Sets the camera look state to <see cref="state"/>.
+		/// </summary>
+		/// <param name="state">The look state.</param>
+		void SetCameraLook(bool state);
+
+		/// <summary>
+		/// Sets the camera turning state to <see cref="state"/>.
+		/// </summary>
+		/// <param name="state">The camera turn state.</param>
+		void SetCameraTurning(bool state);
+	}
+}
