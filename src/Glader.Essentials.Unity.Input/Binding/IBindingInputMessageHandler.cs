64058ee343386d4ddb735978ec67e57eb772909b
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 08:56:53 -0600

    Create IBindingInputMessageHandler.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/IBindingInputMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for a handler for <see cref="KeyBindingInputChangedEventArgs{TBindingEnumType}"/>s.
+	/// </summary>
+	public interface IBindingInputMessageHandler<TKeyBindingEnumType> : IMessageHandler<KeyBindingInputChangedEventArgs<TKeyBindingEnumType>, string> 
+		where TKeyBindingEnumType : Enum
+	{
+		/// <summary>
+		/// The bindings this <see cref="IBindingInputMessageHandler{TKeyBindingEnumType}"/>
+		/// can handle <see cref="KeyBindingInputChangedEventArgs{TBindingEnumType}"/>s for.
+		/// </summary>
+		IEnumerable<TKeyBindingEnumType> Bindings { get; }
+	}
+}
