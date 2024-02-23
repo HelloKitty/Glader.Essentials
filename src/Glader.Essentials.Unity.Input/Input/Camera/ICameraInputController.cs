b7980b439aa86195995ca702d1be17cedd030f67
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:47:06 -0600

    Create ICameraInputController.cs

---
+++ src/Glader.Essentials.Unity.Input/Input/Camera/ICameraInputController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Camera input implementation of <see cref="IAxisInputController"/>
+	/// </summary>
+	public interface ICameraInputController : IAxisInputController
+	{
+		/// <summary>
+		/// Indicates if the camera direction is moving.
+		/// </summary>
+		bool IsCameraDirectionMoving { get; }
+
+		/// <summary>
+		/// Indicates if the camera is in a controlling 
+		/// </summary>
+		bool IsCameraControllingFacing { get; }
+	}
+}
