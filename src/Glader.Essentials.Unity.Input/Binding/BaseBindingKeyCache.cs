9db087bc5052706adddd90e05d351705331be94c
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 12:10:03 -0600

    Create BaseBindingKeyCache.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/BaseBindingKeyCache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Base implementation of <see cref="IBindingKeyCache{TBindingEnumType}"/> where implementers can provide the binding data dictionary.
+	/// </summary>
+	/// <typeparam name="TBindingEnumType">The binding enum type.</typeparam>
+	public abstract class BaseBindingKeyCache<TBindingEnumType> : IBindingKeyCache<TBindingEnumType> 
+		where TBindingEnumType : Enum
+	{
+		private Dictionary<TBindingEnumType, BindingDefinition[]> MappedBindings { get; }
+
+		/// <inheritdoc />
+		public BindingDefinition[] this[TBindingEnumType bind] => MappedBindings[bind];
+
+		/// <summary>
+		/// Creates a new <see cref="BaseBindingKeyCache"/> with the provided bindings data.
+		/// </summary>
+		/// <param name="mappedBindings"></param>
+		protected BaseBindingKeyCache([NotNull] Dictionary<TBindingEnumType, BindingDefinition[]> mappedBindings)
+		{
+			MappedBindings = mappedBindings ?? throw new ArgumentNullException(nameof(mappedBindings));
+		}
+
+		/// <inheritdoc />
+		public KeyCode GetFirstKeyBinding(TBindingEnumType binding)
+		{
+			if(!MappedBindings.TryGetValue(binding, out var bindingInfo))
+				return KeyCode.None;
+
+			if(!bindingInfo.Any())
+				return KeyCode.None;
+
+			if(bindingInfo.First() is KeyBasedBindingDefinition keyBasedBinding)
+				return keyBasedBinding.Code;
+
+			return KeyCode.None;
+		}
+
+		/// <inheritdoc />
+		public bool ContainsKey(TBindingEnumType binding)
+		{
+			return MappedBindings.ContainsKey(binding);
+		}
+
+		/// <inheritdoc />
+		public TBindingEnumType[] DefinedBindList()
+		{
+			return MappedBindings
+				.Keys
+				.Distinct()
+				.ToArray();
+		}
+	}
+}
