b9b25960d362043a41b971e69ee1b722c48b3955
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:32:47 -0600

    Create IAxisInputController.cs

---
+++ src/Glader.Essentials.Unity.Input/Input/IAxisInputController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	// From SwanSong
+	/// <summary>
+	/// Represents a simple axis-based input controller providing Vertical and Horizontal input info.
+	/// </summary>
+	public interface IAxisInputController
+	{
+		/// <summary>
+		/// Horizontal input value.
+		/// </summary>
+		float CurrentHorizontal { get; }
+
+		/// <summary>
+		/// Vertical input value.
+		/// </summary>
+		float CurrentVertical { get; }
+	}
+}
