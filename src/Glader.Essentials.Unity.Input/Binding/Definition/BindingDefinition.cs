5f186c554b28938a737ac18a3c8dcaf07764919b
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 12:04:23 -0600

    Create BindingDefinition.cs

---
+++ src/Glader.Essentials.Unity.Input/Binding/Definition/BindingDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for a binding definition.
+	/// </summary>
+	public abstract record BindingDefinition(params KeyCode[] Modifiers)
+	{
+		/// <summary>
+		/// Indicates if the binding is pressed.
+		/// </summary>
+		/// <returns>True if it's just been pressed.</returns>
+		public abstract bool IsPressed();
+
+		/// <summary>
+		/// Indicates if the binding has been released.
+		/// </summary>
+		/// <returns>True if it's just been released.</returns>
+		public abstract bool IsReleased();
+
+		/// <summary>
+		/// Indicates if the binding has been held.
+		/// </summary>
+		/// <returns></returns>
+		public abstract bool IsHeld();
+	}
+}
