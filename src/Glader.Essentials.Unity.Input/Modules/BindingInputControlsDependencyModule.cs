4d6aab09ce4aec54074082c9d563dd9601995683
HelloKitty <andrew.blakely@ymail.com>
2024-02-23 11:54:31 -0600

    Create BindingInputControlsDependencyModule.cs

---
+++ src/Glader.Essentials.Unity.Input/Modules/BindingInputControlsDependencyModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Glader.Essentials
+{
+	//Based on GladMMO: InputDependencyAutofacModule
+	/// <inheritdoc />
+	public sealed class BindingInputControlsDependencyModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			// For WoW we use the new Binding input
+			// BindingSystemMovementInputController : IBindingSystemMovementInputController
+			builder.RegisterType<BindingSystemMovementInputController>()
+				.As<IMovementInputController>()
+				.As<IBindingSystemMovementInputController>()
+				.SingleInstance();
+
+			builder.RegisterType<BindingSystemInputCameraInputController>()
+				.As<ICameraInputController>()
+				.As<IBindingSystemCameraInputController>()
+				.SingleInstance();
+		}
+	}
+}
