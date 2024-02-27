7e2b03b496eea89926ca892cb324fc0df4f83f16
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 20:15:27 -0600

    Create LazilyDeferredDependency.cs

---
+++ src/Glader.Essentials.AutoFac/Services/LazilyDeferredDependency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	//From SwanSong
+	/// <summary>
+	/// <see cref="Lazy{T}"/>-based implementation of <see cref="IDeferredDependency{TDependencyType}"/>.
+	/// </summary>
+	/// <typeparam name="TDependencyType"></typeparam>
+	public sealed class LazilyDeferredDependency<TDependencyType> : IDeferredDependency<TDependencyType>
+	{
+		private Lazy<TDependencyType> DeferredDependency { get; }
+
+		/// <inheritdoc />
+		public TDependencyType Dependency => DeferredDependency.Value;
+
+		public LazilyDeferredDependency(Lazy<TDependencyType> deferredDependency)
+		{
+			DeferredDependency = deferredDependency ?? throw new ArgumentNullException(nameof(deferredDependency));
+		}
+	}
+}
