1952bbfd1b9cd459d67c73df0e192bc69a700fac
HelloKitty <andrew.blakely@ymail.com>
2024-02-27 00:00:21 -0600

    Create RichTextHelpers.cs

---
+++ src/Glader.Essentials.Unity/Utility/RichTextHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public static class RichTextHelpers
+	{
+		/// <summary>
+		/// Wraps the provided <see cref="text"/> in rich text color tags with the color <see cref="colorDef"/>.
+		/// </summary>
+		/// <param name="text">The text.</param>
+		/// <param name="colorDef">The color.</param>
+		/// <returns>The rich text string wrapped in the color.</returns>
+		public static string WrapTextInColor(string text, UnityColorDefinition colorDef)
+		{
+			return $"<color=#{colorDef.HexCode}>{text}</color>";
+		}
+
+		public static string WrapTextInColor(string text, Color color)
+		{
+			return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{text}</color>";
+		}
+	}
+}
