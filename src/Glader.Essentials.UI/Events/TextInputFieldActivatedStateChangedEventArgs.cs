0db687fc2e6f5de0613c6e10ca4c8fc1ef538d82
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 22:53:28 -0600

    Create TextInputFieldActivatedStateChangedEventArgs.cs

---
+++ src/Glader.Essentials.UI/Events/TextInputFieldActivatedStateChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event fired by text input elements when the activation state of the input field changes.
+	/// </summary>
+	public sealed record TextInputFieldActivatedStateChangedEventArgs(bool Active)
+		: IEventBusEventArgs;
+}
