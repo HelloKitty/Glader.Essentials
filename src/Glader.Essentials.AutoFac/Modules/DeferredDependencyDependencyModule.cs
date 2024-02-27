ecf9e21dd534f8af2893dc244d33b1dae2f68897
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 20:19:57 -0600

    Create DeferredDependencyDependencyModule.cs

---
+++ src/Glader.Essentials.AutoFac/Modules/DeferredDependencyDependencyModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Registers <see cref="IDeferredDependency{TDependencyType}"/> using the <see cref="LazilyDeferredDependency{TDependencyType}"/>.
+	/// </summary>
+	public sealed class DeferredDependencyLazyDependencyModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			// For lazily loadded/deferred dependencies
+			builder.RegisterGeneric(typeof(LazilyDeferredDependency<>))
+				.As(typeof(IDeferredDependency<>))
+				.SingleInstance();
+		}
+	}
+}
