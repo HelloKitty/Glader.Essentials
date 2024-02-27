5cbf4967f1b67782c66c90313d11a68b87f4a6c1
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 22:29:19 -0600

    Added IsFocused and CaretWidth to the UnityTextMeshproUGUIInputFieldAdapter

--- src/Glader.Essentials.Unity/UI/Abstraction/Adapters/TextMeshPro/UnityTextMeshProUGUIUIInputFieldAdapter.cs
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/TextMeshPro/UnityTextMeshProUGUIUIInputFieldAdapter.cs
@@ -8,6 +8,20 @@ namespace Glader.Essentials
 {
 	public class UnityTextMeshProUGUIUIInputFieldAdapter : BaseUnityInputFieldAdapter<TMP_InputField>
 	{
+		/// <summary>
+		/// Indicates if the input is focused.
+		/// </summary>
+		public bool IsFocused => UnityUIObject.isFocused;
+
+		/// <summary>
+		/// The width of the caret.
+		/// </summary>
+		public int CaretWidth
+		{
+			get => UnityUIObject.caretWidth;
+			set => UnityUIObject.caretWidth = value;
+		}
+
 		public override void SetColor(byte r, byte g, byte b, byte a)
 		{
 			UnityUIObject.textComponent.color = new Color32(r, g, b, a);
