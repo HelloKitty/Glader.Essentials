5374cdf754138f219801deacda3c1ed469c915d5
HelloKitty <andrew.blakely@ymail.com>
2024-02-27 00:00:23 -0600

    Create RichTextExtensions.cs

---
+++ src/Glader.Essentials.Unity/Extensions/RichTextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public static class RichTextExtensions
+	{
+		/// <summary>
+		/// Extension to wrarp the provided text <see cref="value"/> in the rich color tags for the <see cref="colorDef"/>.
+		/// </summary>
+		/// <param name="value">The string value.</param>
+		/// <param name="colorDef">The color.</param>
+		/// <returns>The text wrapped in the rich text tags for the provided color.</returns>
+		public static string WrapInColor(this string value, UnityColorDefinition colorDef)
+		{
+			return RichTextHelpers.WrapTextInColor(value, colorDef);
+		}
+	}
+}
