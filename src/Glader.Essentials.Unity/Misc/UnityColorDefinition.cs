5c64df4f56baa7be550b2caabe966dc48d18d576
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 23:34:40 -0600

    Create UnityColorDefinition.cs

---
+++ src/Glader.Essentials.Unity/Misc/UnityColorDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Represents the definition of a color including the <see cref="Color"/> and the <see cref="HexCode"/>.
+	/// </summary>
+	public sealed record UnityColorDefinition(Color Color, string HexCode)
+	{
+		/// <summary>
+		/// Creates a new <see cref="UnityColorDefinition"/> with the provided <see cref="color"/>.
+		/// </summary>
+		/// <param name="color">The color.</param>
+		/// <returns>A <see cref="UnityColorDefinition"/> based on the provided <see cref="color"/>.</returns>
+		public static UnityColorDefinition Create(Color color)
+		{
+			return new UnityColorDefinition(color, ColorUtility.ToHtmlStringRGB(color));
+		}
+	}
+}
