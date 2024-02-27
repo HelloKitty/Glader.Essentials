571a4935a6a37c169460c88576e075ddb1a937e0
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 20:14:01 -0600

    Create IDeferredDependency.cs

---
+++ src/Glader.Essentials.AutoFac/Services/IDeferredDependency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	//From SwanSong
+	/// <summary>
+	/// Contract for an object that provides access to a dependency in a deferred way.
+	/// </summary>
+	/// <typeparam name="TDependencyType">The dependency type.</typeparam>
+	public interface IDeferredDependency<out TDependencyType>
+	{
+		/// <summary>
+		/// The dependency.
+		/// </summary>
+		TDependencyType Dependency { get; }
+	}
+}
