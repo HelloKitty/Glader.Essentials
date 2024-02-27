82ef07f7d3c893e0a6fba3111e3498889695fb32
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 23:51:45 -0600

    Create UnityColorHelpers.cs

---
+++ src/Glader.Essentials.Unity/Utility/UnityColorHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glader.Essentials
+{
+	public static class UnityColorHelpers
+	{
+		/// <summary>
+		/// Creates a color with the specified components.
+		/// </summary>
+		/// <param name="r"></param>
+		/// <param name="g"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static Color CreateColor(float r, float g, float b, float a = 1.0f)
+		{
+			return new Color(r, g, b, a);
+		}
+
+		/// <summary>
+		/// Creates a color with the specified components.
+		/// </summary>
+		/// <param name="r"></param>
+		/// <param name="g"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static Color CreateColor(double r, double g, double b)
+		{
+			return CreateColor((float)r, (float)g, (float)b);
+		}
+	}
+}
