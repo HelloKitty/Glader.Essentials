66d8b08ce3009527dfc5897cb638906ffb33c2d6
HelloKitty <andrew.blakely@ymail.com>
2024-02-26 22:34:43 -0600

    Create IUITextChangedCallbackRegistrable.cs

---
+++ src/Glader.Essentials.UI/Interface/IUITextChangedCallbackRegistrable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Interface for types that provide a callback mechanism for listening to text changes.
+	/// </summary>
+	public interface IUITextChangedCallbackRegistrable
+	{
+		/// <summary>
+		/// Registers a <see cref="callback"/> which is called when the underlying text changes.
+		/// </summary>
+		/// <param name="callback">The callback.</param>
+		void RegisterTextChangeCallback(Action<string> callback);
+	}
+}
