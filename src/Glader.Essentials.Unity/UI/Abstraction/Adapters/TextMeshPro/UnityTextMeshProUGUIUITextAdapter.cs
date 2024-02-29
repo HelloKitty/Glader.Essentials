f82e81b78d4f5a25916635a12e3e22b5a7a65c25
HelloKitty <andrew.blakely@ymail.com>
2024-02-29 14:15:44 -0600

    Added link event to TextMeshProUGUITextAdapter

--- src/Glader.Essentials.Unity/UI/Abstraction/Adapters/TextMeshPro/UnityTextMeshProUGUIUITextAdapter.cs
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/TextMeshPro/UnityTextMeshProUGUIUITextAdapter.cs
@@ -4,10 +4,11 @@ using System.Text;
 using Glader.Essentials;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
 namespace Glader.Essentials
 {
-	public class UnityTextMeshProUGUIUITextAdapter : BaseUnityUIAdapter<TextMeshProUGUI, IUIText>, IUIText
+	public class UnityTextMeshProUGUIUITextAdapter : BaseUnityUIAdapter<TextMeshProUGUI, IUIText>, IUIText, IPointerClickHandler
 	{
 		public string Text
 		{
@@ -19,6 +20,18 @@ namespace Glader.Essentials
 		public void SetColor(byte r, byte g, byte b, byte a)
 		{
 			UnityUIObject.color = new Color32(r, g, b, a);
+		}
+
+		/// <inheritdoc />
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			// Really this should only be for links
+			int linkIndex = TMP_TextUtilities.FindIntersectingLink(UnityUIObject, eventData.position, null);
+			if (linkIndex != -1)
+			{
+				TMP_LinkInfo linkInfo = UnityUIObject.textInfo.linkInfo[linkIndex];
+				Bus.Publish(this, new TextLinkClickedEventArgs(eventData.button.ToMouseButtonType(), linkInfo.GetLinkID(), linkInfo.GetLinkText()));
+			}
 		}
 	}
 }
