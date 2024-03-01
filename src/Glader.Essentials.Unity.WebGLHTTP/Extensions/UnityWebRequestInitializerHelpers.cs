ca96634ede3d852b0d078fa656f80d5dccff9a11
HelloKitty <andrew.blakely@ymail.com>
2024-03-01 15:33:13 -0600

    Added CreateRequest to the UnityWebRequestInitializerHelpers

--- src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestInitializerHelpers.cs
+++ src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestInitializerHelpers.cs
@@ -31,5 +31,34 @@ namespace Glader.Essentials
 			if (request == null) throw new ArgumentNullException(nameof(request));
 			request.certificateHandler = DisabledCertificateHandler.Instance;
 		}
+
+		/// <summary>
+		/// Creates a <see cref="UnityWebRequest"/> for the provided HTTP <see cref="type"/> with the provided <see cref="url"/> and <see cref="data"/>.
+		/// </summary>
+		/// <param name="type">The HTTP type. (Ex. GET)</param>
+		/// <param name="url">The url/uri</param>
+		/// <param name="data">The request data. Used for Post/Put.</param>
+		/// <returns>A unsent <see cref="UnityWebRequest"/>. Call must send.</returns>
+		public static UnityWebRequest CreateRequest(UnityHttpRequestType type, string url, string data = null)
+		{
+			switch (type)
+			{
+				case UnityHttpRequestType.Delete:
+					return UnityWebRequest.Delete(url);
+				case UnityHttpRequestType.Get:
+					return UnityWebRequest.Get(url);
+				case UnityHttpRequestType.Head:
+					return UnityWebRequest.Head(url);
+				case UnityHttpRequestType.Post:
+					return UnityWebRequest.Post(url, data);
+				case UnityHttpRequestType.Put:
+					return UnityWebRequest.Put(url, data);
+				case UnityHttpRequestType.Option:
+				case UnityHttpRequestType.Patch:
+				case UnityHttpRequestType.Trace:
+				default:
+					throw new ArgumentOutOfRangeException(nameof(type), type, null);
+			}
+		}
 	}
 }
