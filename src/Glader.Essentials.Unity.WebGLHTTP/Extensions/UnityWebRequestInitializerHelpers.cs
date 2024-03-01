463d051eb37929bc4c862f85c56c6e435db39a1c
HelloKitty <andrew.blakely@ymail.com>
2024-03-01 15:27:13 -0600

    Create UnityWebRequestInitializerHelpers.cs

---
+++ src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestInitializerHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using UnityEngine.Networking;
+
+namespace Glader.Essentials
+{
+	public static class UnityWebRequestInitializerHelpers
+	{
+		internal sealed class DisabledCertificateHandler : CertificateHandler
+		{
+			/// <summary>
+			/// Shared <see cref="DisabledCertificateHandler"/> instance.
+			/// </summary>
+			public static CertificateHandler Instance { get; } = new DisabledCertificateHandler();
+
+			/// <inheritdoc />
+			protected override bool ValidateCertificate(byte[] certificateData)
+			{
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Disables the certificate checks for the provided <see cref="request"/>.
+		/// </summary>
+		/// <param name="request">The request to disable the cert checks for.</param>
+		public static void DisableCertCheck([NotNull] UnityWebRequest request)
+		{
+			if (request == null) throw new ArgumentNullException(nameof(request));
+			request.certificateHandler = DisabledCertificateHandler.Instance;
+		}
+	}
+}
