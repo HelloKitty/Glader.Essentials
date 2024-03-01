fee7a92e105a52acb621e465fe3adff21748aa59
HelloKitty <andrew.blakely@ymail.com>
2024-03-01 15:29:35 -0600

    Create UnityHttpRequestType.cs

---
+++ src/Glader.Essentials.Unity.WebGLHTTP/UnityHttpRequestType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public enum UnityHttpRequestType
+	{
+		Delete =1,
+		Get,
+		Head,
+		Option,
+		Patch,
+		Post,
+		Put, 
+		Trace,
+	}
+}
