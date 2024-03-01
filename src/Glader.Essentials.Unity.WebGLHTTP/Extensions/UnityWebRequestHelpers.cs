dd24a23d41e508920a42c25c54611bbc6b45c605
HelloKitty <andrew.blakely@ymail.com>
2024-03-01 15:52:33 -0600

    Added XML and JSON methods for UnityWebRequest

--- src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestHelpers.cs
+++ src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestHelpers.cs
@@ -6,12 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Glader.Essentials;
+using Newtonsoft.Json;
 using UnityEngine.Networking;
 
 namespace Glader.Essentials
 {
 	public static class UnityWebRequestHelpers
 	{
+		private static class XmlSerializerHelper<T>
+		{
+			public static XmlSerializer TypeSerializer { get; } = new XmlSerializer(typeof(T));
+		}
+
 		// TODO: Doc
 		/// <summary>
 		/// Requests bytes from the provided <see cref="url"/> with the specified args.
@@ -29,6 +35,59 @@ namespace Glader.Essentials
 				if (request.result == UnityWebRequest.Result.Success)
 				{
 					token.SetResult(request.downloadHandler.data);
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
+
+		/// <summary>
+		/// Requests XML deserialized result from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<TResponseBodyType> RequestXml<TResponseBodyType>(UnityHttpRequestType type, string url, string data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<TResponseBodyType>(type, url, data, out var token);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					using(var stream = new StringReader(request.downloadHandler.text))
+						token.SetResult((TResponseBodyType)XmlSerializerHelper<TResponseBodyType>.TypeSerializer.Deserialize(stream));
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
+
+		/// <summary>
+		/// Requests XML deserialized result from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<TResponseBodyType> RequestJson<TResponseBodyType>(UnityHttpRequestType type, string url, string data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<TResponseBodyType>(type, url, data, out var token);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					token.SetResult(JsonConvert.DeserializeObject<TResponseBodyType>(request.downloadHandler.text));
 				}
 				else
 				{
