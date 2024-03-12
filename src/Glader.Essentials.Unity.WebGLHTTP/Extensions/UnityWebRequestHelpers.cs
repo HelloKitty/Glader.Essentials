2137e65b96ec28275e50b56f9d135ee3f88dd656
HelloKitty <andrew.blakely@ymail.com>
2024-03-11 21:56:32 -0500

    Added byte array overloads for the UnityWebRequest library

--- src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestHelpers.cs
+++ src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestHelpers.cs
@@ -135,5 +135,124 @@ namespace Glader.Essentials
 
 			return token.Task;
 		}
+
+		// Bytes version
+		// TODO: Doc
+		/// <summary>
+		/// Requests bytes from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="requestConfigAction"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<byte[]> RequestBytes(UnityHttpRequestType type, string url,
+			Action<UnityWebRequest> requestConfigAction = null, byte[] data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<byte[]>(type, url, data, out var token);
+			requestConfigAction?.Invoke(request);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					token.SetResult(request.downloadHandler.data);
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
+
+		/// <summary>
+		/// Requests XML deserialized result from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="requestConfigAction"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<TResponseBodyType> RequestXml<TResponseBodyType>(UnityHttpRequestType type, string url,
+			Action<UnityWebRequest> requestConfigAction = null, byte[] data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<TResponseBodyType>(type, url, data, out var token);
+			requestConfigAction?.Invoke(request);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					using(var stream = new StringReader(request.downloadHandler.text))
+						token.SetResult((TResponseBodyType)XmlSerializerHelper<TResponseBodyType>.TypeSerializer.Deserialize(stream));
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
+
+		/// <summary>
+		/// Requests JSON deserialized result from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="requestConfigAction"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<TResponseBodyType> RequestJson<TResponseBodyType>(UnityHttpRequestType type, string url,
+			Action<UnityWebRequest> requestConfigAction = null, byte[] data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<TResponseBodyType>(type, url, data, out var token);
+			requestConfigAction?.Invoke(request);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					token.SetResult(JsonConvert.DeserializeObject<TResponseBodyType>(request.downloadHandler.text));
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
+
+		/// <summary>
+		/// Requests the string result from the provided <see cref="url"/> with the specified args.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="url"></param>
+		/// <param name="requestConfigAction"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static Task<string> RequestString(UnityHttpRequestType type, string url,
+			Action<UnityWebRequest> requestConfigAction = null, byte[] data = null)
+		{
+			var request = UnityWebRequestInitializerHelpers.CreateRequestWithCallbackToken<string>(type, url, data, out var token);
+			requestConfigAction?.Invoke(request);
+
+			request.SendWebRequest().completed += operation =>
+			{
+				if(request.result == UnityWebRequest.Result.Success)
+				{
+					token.SetResult(request.downloadHandler.text);
+				}
+				else
+				{
+					token.SetException(new InvalidOperationException($"Failed to handle request at Endpoint: {url} Result: {request.result} Error: {request.error}"));
+				}
+			};
+
+			return token.Task;
+		}
 	}
 }
--- src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestInitializerHelpers.cs
+++ src/Glader.Essentials.Unity.WebGLHTTP/Extensions/UnityWebRequestInitializerHelpers.cs
@@ -81,5 +81,61 @@ namespace Glader.Essentials
 			UnityWebRequestInitializerHelpers.DisableCertCheck(request);
 			return request;
 		}
+
+		/// <summary>
+		/// Creates a <see cref="UnityWebRequest"/> for the provided HTTP <see cref="type"/> with the provided <see cref="url"/> and <see cref="data"/>.
+		/// </summary>
+		/// <param name="type">The HTTP type. (Ex. GET)</param>
+		/// <param name="url">The url/uri</param>
+		/// <param name="data">The request data. Used for Post/Put.</param>
+		/// <returns>A unsent <see cref="UnityWebRequest"/>. Call must send.</returns>
+		public static UnityWebRequest CreateRequest(UnityHttpRequestType type, string url, byte[] data = null)
+		{
+			switch(type)
+			{
+				case UnityHttpRequestType.Delete:
+					return UnityWebRequest.Delete(url);
+				case UnityHttpRequestType.Get:
+					return UnityWebRequest.Get(url);
+				case UnityHttpRequestType.Head:
+					return UnityWebRequest.Head(url);
+				case UnityHttpRequestType.Post:
+				{
+					var uwr = new UnityWebRequest(url);
+					uwr.method = "POST";
+					uwr.uploadHandler = new UploadHandlerRaw(data);
+					uwr.downloadHandler = new DownloadHandlerBuffer();
+
+					return uwr;
+				}
+				case UnityHttpRequestType.Put:
+					return UnityWebRequest.Put(url, data);
+				case UnityHttpRequestType.Option:
+				case UnityHttpRequestType.Patch:
+				case UnityHttpRequestType.Trace:
+				default:
+					throw new ArgumentOutOfRangeException(nameof(type), type, null);
+			}
+		}
+
+		/// <summary>
+		/// Creates a <see cref="UnityWebRequest"/> for the provided HTTP <see cref="type"/> with the provided <see cref="url"/> and <see cref="data"/>.
+		/// Will also create a usable callback token <see cref="TaskCompletionSource{TResult}"/> for the request.
+		/// </summary>
+		/// <typeparam name="TCallbackType">The callback type.</typeparam>
+		/// <param name="type">The HTTP type. (Ex. GET)</param>
+		/// <param name="url">The url/uri</param>
+		/// <param name="data">The request data. Used for Post/Put.</param>
+		/// <param name="callbackToken">The callback token.</param>
+		/// <returns>A unsent <see cref="UnityWebRequest"/>. Call must send. Also an incomplete callback token.</returns>
+		public static UnityWebRequest CreateRequestWithCallbackToken<TCallbackType>(UnityHttpRequestType type,
+			string url, byte[] data, out TaskCompletionSource<TCallbackType> callbackToken)
+		{
+			callbackToken = new TaskCompletionSource<TCallbackType>();
+
+			var request = UnityWebRequestInitializerHelpers.CreateRequest(type, url, data);
+			UnityWebRequestInitializerHelpers.DisableCertCheck(request);
+			return request;
+		}
 	}
 }
