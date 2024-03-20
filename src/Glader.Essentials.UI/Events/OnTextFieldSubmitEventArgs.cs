f0ec8cf08cf50fc0ef309eb7f994848ab0928c1e
HelloKitty <andrew.blakely@ymail.com>
2024-03-20 03:00:03 -0500

    Create OnTextFieldSubmitEventArgs.cs

---
+++ src/Glader.Essentials.UI/Events/OnTextFieldSubmitEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader
+{
+	/// <summary>
+	/// Event fired by the framework when a TextField has a submit event.
+	/// </summary>
+	public sealed record OnTextFieldSubmitEventArgs(IUIInputField InputField, string Text)
+		: IEventBusEventArgs;
+}
