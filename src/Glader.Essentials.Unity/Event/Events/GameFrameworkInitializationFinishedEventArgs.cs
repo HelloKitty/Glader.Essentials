12b998b292c243a4c6521f07eeaf069ec94c97cd
HelloKitty <andrew.blakely@ymail.com>
2024-06-12 02:06:56 -0500

    Create GameFrameworkInitializationFinishedEventArgs.cs

---
+++ src/Glader.Essentials.Unity/Event/Events/GameFrameworkInitializationFinishedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials.Unity;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Event fired by the <see cref="DefaultGameFrameworkManager"/> after the game framework has been initialized.
+	/// </summary>
+	public sealed record GameFrameworkInitializationFinishedEventArgs()
+		: IEventBusEventArgs
+	{
+		/// <summary>
+		/// Shared instance for the init event.
+		/// </summary>
+		public static GameFrameworkInitializationFinishedEventArgs Instance { get; } = new();
+	}
+}
