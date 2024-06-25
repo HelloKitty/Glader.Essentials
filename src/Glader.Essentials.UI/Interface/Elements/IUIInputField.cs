9178d3e05586aee800ee96f5b73197b706365ea5
HelloKitty <andrew.blakely@ymail.com>
2024-06-25 11:49:46 -0500

    Remove old link rich text removal attermpt

--- src/Glader.Essentials.UI/Interface/Elements/IUIInputField.cs
+++ src/Glader.Essentials.UI/Interface/Elements/IUIInputField.cs
@@ -31,17 +31,8 @@ namespace Glader.Essentials
 		void InsertText(string text);
 
 		/// <summary>
-		/// Checks if the caret is within a rich text tag or right after a rich text tag.
-		/// </summary>
-		/// <param name="isWithinTag">Output parameter that is true if the caret is within a rich text tag.</param>
-		/// <param name="isRightAfterTag">Output parameter that is true if the caret is right after a rich text tag.</param>
-		/// <returns>True if any of the parameters are not false.</returns>
-		bool CheckCaretRichTextTagState(out bool isWithinTag, out bool isRightAfterTag);
-
-		/// <summary>
 		/// Attempts to remove a rich text block if the cursor is within or right behind it.
 		/// It will do nothing if it doesn't find any RichText that matches that condition.
-		/// Will check <see cref="CheckCaretRichTextTagState"/>.
 		/// </summary>
 		/// <returns>True if a rich text block was removed.</returns>
 		bool TryRemoveRichTextBlock();
