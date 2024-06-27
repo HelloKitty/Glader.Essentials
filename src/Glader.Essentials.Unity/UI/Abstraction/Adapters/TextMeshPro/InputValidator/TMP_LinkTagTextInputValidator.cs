ba6778763ea3ff572ea2f3ba9761d183e6d54f26
HelloKitty <andrew.blakely@ymail.com>
2024-06-27 10:32:54 -0500

    Create TMP_LinkTagTextInputValidator.cs

---
+++ src/Glader.Essentials.Unity/UI/Abstraction/Adapters/TextMeshPro/InputValidator/TMP_LinkTagTextInputValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using TMPro;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// <see cref="TMP_InputValidator"/> which prevents inserting characters within a string
+	/// if 
+	/// </summary>
+	public sealed class TMP_LinkTagTextInputValidator : TMP_InputValidator
+	{
+		private IUIInputField InputField { get; }
+
+		public TMP_LinkTagTextInputValidator([NotNull] IUIInputField inputField)
+		{
+			InputField = inputField ?? throw new ArgumentNullException(nameof(inputField));
+		}
+
+		/// <inheritdoc />
+		public override char Validate(ref string text, ref int pos, char ch)
+		{
+			// Don't allow any typing when within a link
+			if (InputField.IsWithinLinkTagAtPosition(text, pos))
+				return '\0';
+
+			pos += 1;
+			return ch;
+		}
+	}
+}
