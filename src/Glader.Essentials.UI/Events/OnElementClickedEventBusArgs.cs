debf6b5a0b64ad786037290ce612fd2d13b1759b
HelloKitty <andrew.blakely@ymail.com>
2024-07-20 03:06:13 -0500

    Added MouseButton and Down info in the element click button event

--- src/Glader.Essentials.UI/Events/OnElementClickedEventBusArgs.cs
+++ src/Glader.Essentials.UI/Events/OnElementClickedEventBusArgs.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine.UIElements;
 
 namespace Glader.Essentials
 {
 	/// <summary>
 	/// Event fired by the UI system when a clickable element has been clicked.
 	/// </summary>
-	public record OnElementClickedEventArgs : IEventBusEventArgs;
+	public record OnElementClickedEventArgs(MouseButton Button, bool IsDown) 
+		: IEventBusEventArgs;
 }
