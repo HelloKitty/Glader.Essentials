32acecfb428d52c782ecb2cdf234f53fa1bb7816
HelloKitty <andrew.blakely@ymail.com>
2024-07-20 03:57:23 -0500

    Added OnClickDown and OnClickUp event register extensions

--- src/Glader.Essentials.UI/Interface/Extensions/IUIElementExtensions.cs
+++ src/Glader.Essentials.UI/Interface/Extensions/IUIElementExtensions.cs
@@ -17,12 +17,45 @@ namespace Glader.Essentials
 
 		/// <summary>
 		/// Subscribes a callback for the button's click.
+		/// (Fires for both up and down)
 		/// </summary>
 		/// <param name="element">The button to subscribe to.</param>
 		/// <param name="callback">The callback to register.</param>
 		public static EventBusSubscriptionToken OnClick(this IUIClickable element, Action<object, OnElementClickedEventArgs> callback)
 		{
 			return CreateSubscription(element, callback);
+		}
+
+		/// <summary>
+		/// Subscribes a callback for the button's click.
+		/// </summary>
+		/// <param name="element">The button to subscribe to.</param>
+		/// <param name="callback">The callback to register.</param>
+		public static EventBusSubscriptionToken OnClickDown(this IUIClickable element, Action<object, OnElementClickedEventArgs> callback)
+		{
+			return CreateSubscription(element, (object s, OnElementClickedEventArgs args) =>
+			{
+				if (!args.IsDown)
+					return;
+
+				callback?.Invoke(s, args);
+			});
+		}
+
+		/// <summary>
+		/// Subscribes a callback for the button's click.
+		/// </summary>
+		/// <param name="element">The button to subscribe to.</param>
+		/// <param name="callback">The callback to register.</param>
+		public static EventBusSubscriptionToken OnClickUp(this IUIClickable element, Action<object, OnElementClickedEventArgs> callback)
+		{
+			return CreateSubscription(element, (object s, OnElementClickedEventArgs args) =>
+			{
+				if (args.IsDown)
+					return;
+
+				callback?.Invoke(s, args);
+			});
 		}
 
 		/// <summary>
