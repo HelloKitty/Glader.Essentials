b8cd9f67168f16721403864e1dbed542c1404a8f
HelloKitty <andrew.blakely@ymail.com>
2024-07-29 14:50:39 -0500

    Create EmptyDeferredDependency.cs

---
+++ src/Glader.Essentials.AutoFac/Services/EmptyDeferredDependency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Empty/Null implementation of <see cref="IDeferredDependency{TDependencyType}"/>
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public sealed class EmptyDeferredDependency<T> : IDeferredDependency<T>
+	{
+		/// <summary>
+		/// Instance of an empty.
+		/// </summary>
+		public static IDeferredDependency<T> Instance { get; } = new EmptyDeferredDependency<T>();
+
+		/// <inheritdoc />
+		public T Dependency => default;
+	}
+}
