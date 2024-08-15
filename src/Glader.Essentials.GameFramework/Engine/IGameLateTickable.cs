7514df1b4a570acbacbc861b024188f6248be736
HelloKitty <andrew.blakely@ymail.com>
2024-08-15 06:45:39 -0500

    Create IGameLateTickable.cs

---
+++ src/Glader.Essentials.GameFramework/Engine/IGameLateTickable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Contract for services/components that need to be ticked
+	/// forward in time at the end of the frame.
+	/// </summary>
+	public interface IGameLateTickable
+	{
+		/// <summary>
+		/// Called to tick the game forward at the end of the frame.
+		/// </summary>
+		void LateTick();
+	}
+}
