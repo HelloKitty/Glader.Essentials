847d7b811a73256a5b8c97fd9db1980668abb639
HelloKitty <andrew.blakely@ymail.com>
2024-08-18 02:35:50 -0500

    Added GetOrAddComponent extension for GameObjects

---
+++ src/Glader.Essentials.Unity/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Glader
+{
+	public static class GameObjectExtensions
+	{
+		/// <summary>
+		/// Gets or adds the component.
+		/// Will try to get it but if it doesn't exist will call AddComponent.
+		/// </summary>
+		/// <typeparam name="TComponentType">The component type.</typeparam>
+		/// <param name="go">The gameobject.</param>
+		/// <returns>The component.</returns>
+		public static TComponentType GetOrAddComponent<TComponentType>([NotNull] this GameObject go) 
+			where TComponentType : Component
+		{
+			if (go == null) throw new ArgumentNullException(nameof(go));
+
+			if (go.TryGetComponent<TComponentType>(out var comp))
+				return comp;
+
+			return go.AddComponent<TComponentType>();
+		}
+	}
+}
