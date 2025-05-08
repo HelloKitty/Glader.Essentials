3c8f55fc147e35fb6cf9df282849d0deb84f008e
HelloKitty <andrew.blakely@ymail.com>
2025-05-08 01:51:45 -0500

    Added GladerEssentialsGameFrameworkUnityConstants

---
+++ src/Glader.Essentials.Unity/Constants/GladerEssentialsGameFrameworkUnityConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public static class GladerEssentialsGameFrameworkUnityConstants
+	{
+		/// <summary>
+		/// If enabled the game framework will report timing diagnostics.
+		/// </summary>
+		public static bool GAME_FRAMEWORK_TIMING_ENABLED { get; set; } = true;
+	}
+}
