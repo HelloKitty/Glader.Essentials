3ab9c09f8c01ac7104499d6bd4ee4e1fdb86b0e8
HelloKitty <andrew.blakely@ymail.com>
2025-06-21 12:04:13 -0500

    Added non-async timing util for PerformTimedAction

--- src/Glader.Essentials.Common/Utility/GladerTimingUtils.cs
+++ src/Glader.Essentials.Common/Utility/GladerTimingUtils.cs
@@ -66,5 +66,35 @@ namespace Glader.Essentials
 					watch.Stop();
 			}
 		}
+
+		/// <summary>
+		/// Times an action and returns the time.
+		/// </summary>
+		/// <param name="func">The function.</param>
+		/// <param name="timerName">The name of the timer.</param>
+		/// <param name="logFunc">The logging function.</param>
+		/// <returns></returns>
+		public static void PerformTimedAction(Action func, string timerName, Action<TimeSpan, string> logFunc)
+		{
+			Stopwatch watch = new();
+			watch.Start();
+
+			try
+			{
+				func();
+
+				watch.Stop();
+				logFunc(watch.Elapsed, $"Timed {timerName}: {watch.Elapsed.TotalSeconds}");
+			}
+			catch(InvalidOperationException e)
+			{
+				throw new InvalidOperationException($"Failed to finish timed Task: {timerName}. Reason: {e}", e);
+			}
+			finally
+			{
+				if(watch.IsRunning)
+					watch.Stop();
+			}
+		}
 	}
 }
