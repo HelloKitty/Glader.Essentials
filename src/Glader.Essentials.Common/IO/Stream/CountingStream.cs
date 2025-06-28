f0acd3744a4c482aed5b7094e24c0d7d2b5e9f6c
HelloKitty <andrew.blakely@ymail.com>
2025-06-28 17:05:31 -0500

    Added CountingStream to allow reading total bytes read from certain streams like NetworkStream

---
+++ src/Glader.Essentials.Common/IO/Stream/CountingStream.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Glader.Essentials
+{
+	/// <summary>
+	/// Decorator for wrapping <see cref="Stream"/> with reliable counting semantics.
+	/// (Ex. <see cref="NetworkStream"/> will not allow for calling Position).
+	/// See: <see cref="BytesRead"/>.
+	/// </summary>
+	public class CountingStream : Stream
+	{
+		private readonly Stream _inner;
+		private long _bytesRead;
+
+		/// <summary>
+		/// Represents the bytes read from the stream.
+		/// </summary>
+		public long BytesRead => Interlocked.Read(ref _bytesRead);
+
+		/// <inheritdoc />
+		public CountingStream(Stream inner)
+		{
+			_inner = inner ?? throw new ArgumentNullException(nameof(inner));
+		}
+
+		/// <inheritdoc />
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			var n = _inner.Read(buffer, offset, count);
+			if(n > 0) Interlocked.Add(ref _bytesRead, n);
+			return n;
+		}
+
+		/// <inheritdoc />
+		public override async Task<int> ReadAsync(
+			byte[] buffer, int offset, int count, CancellationToken token)
+		{
+			var n = await _inner.ReadAsync(buffer, offset, count, token).ConfigureAwait(false);
+			if(n > 0) Interlocked.Add(ref _bytesRead, n);
+			return n;
+		}
+
+		/// <inheritdoc />
+		public override bool CanRead => _inner.CanRead;
+
+		/// <inheritdoc />
+		public override bool CanSeek => false;
+
+		/// <inheritdoc />
+		public override bool CanWrite => _inner.CanWrite;
+
+		/// <inheritdoc />
+		public override long Length => _inner.Length;
+
+		/// <inheritdoc />
+		public override long Position
+		{
+			get => BytesRead;
+			set => throw new NotSupportedException();
+		}
+
+		/// <inheritdoc />
+		public override void Flush() => _inner.Flush();
+
+		/// <inheritdoc />
+		public override long Seek(long o, SeekOrigin w)
+			=> throw new NotSupportedException();
+
+		/// <inheritdoc />
+		public override void SetLength(long value)
+			=> _inner.SetLength(value);
+
+		/// <inheritdoc />
+		public override void Write(byte[] buffer, int offset, int count)
+			=> _inner.Write(buffer, offset, count);
+		/// <inheritdoc />
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing) _inner.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
