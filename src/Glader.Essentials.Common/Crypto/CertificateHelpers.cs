3b77894709f03d07ea5e0c0e257ecea6e31b57ba
HelloKitty <andrew.blakely@ymail.com>
2025-06-30 19:24:59 -0500

    Removed non-working certificatehelpers

---
+++ src/Glader.Essentials.Common/Crypto/CertificateHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Glader.Essentials
+{
+	public static class CertificateHelpers
+	{
+		
+	}
+}
